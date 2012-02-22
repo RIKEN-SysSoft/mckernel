0be6147a81a49a96e384488d31b2c0149e661dbd
Taku Shimosawa <shimosawa@is.s.u-tokyo.ac.jp>
2012-02-22 18:01:55 +0900

    DMA copying now specifies destination operating system

--- kernel/copy.c
+++ kernel/copy.c
@@ -10,14 +10,19 @@ int memcpy_async(unsigned long dest, unsigned long src,
 	if (notify)
 		*notify = 0;
 	memset(&req, 0, sizeof(req));
+	/* Physical */
+	req.src_os = (aal_os_t)AAL_THIS_OS;
 	req.src_phys = src;
+	req.dest_os = (aal_os_t)AAL_THIS_OS;
 	req.dest_phys = dest;
 	req.size = len;
 
 	if (notify) {
+		req.notify_os = (aal_os_t)AAL_THIS_OS;
 		req.notify = (void *)virt_to_phys(notify);
 		req.priv = (void *)1;
 	} else if (wait) {
+		req.notify_os = (aal_os_t)AAL_THIS_OS;
 		req.notify = (void *)virt_to_phys(&fin);
 		req.priv = (void *)1;
 	}
