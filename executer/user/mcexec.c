a5b8d388e9773b547f08ccd458a918bc1225f5d1
Balazs Gerofi bgerofi@riken.jp <bgerofi@kncc-login1.(none)>
2014-05-12 10:30:39 +0900

    dprintf exit status

--- executer/user/mcexec.c
+++ executer/user/mcexec.c
@@ -1084,7 +1084,7 @@ int main_loop(int fd, int cpu, pthread_mutex_t *lock)
 					if(sig)
 						fprintf(stderr, "Terminate by signal %d\n", sig);
 					else if(term)
-						fprintf(stderr, "Exit status: %d\n", term);
+						__dprintf(stderr, "Exit status: %d\n", term);
 				}
 			}
 
