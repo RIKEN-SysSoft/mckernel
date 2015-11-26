e8052496519f347e7c2e15147eae329fc40e2e44
NAKAMURA Gou <go.nakamura.yw@hitachi-solutions.com>
2015-11-26 20:07:26 +0900

    add strrchr()

--- lib/string.c
+++ lib/string.c
@@ -95,7 +95,20 @@ char *strchr(const char *s, int n) {
 	}
 	return NULL;
 }
-		
+
+char *
+strrchr(const char *s, int c)
+{
+	const char *last = NULL;
+
+	do {
+		if (*s == c) {
+			last = s;
+		}
+	} while (*(s++));
+
+	return (char *)last;
+} /* strrchr() */
 
 char *strstr(const char *haystack, const char *needle)
 {
