b3ae7f46bd35f092feedaf0d04e7d329ac53f677
NAKAMURA Gou <go.nakamura.yw@hitachi-solutions.com>
2016-03-10 20:19:03 +0900

    add rlim_t (a type of rlim_cur and rlim_max)

--- kernel/include/generic-rlimit.h
+++ kernel/include/generic-rlimit.h
@@ -13,9 +13,11 @@
 #ifndef __GENERIC_RLIMIT_H
 #define __GENERIC_RLIMIT_H
 
+typedef uint64_t rlim_t;
+
 struct rlimit {
-	uint64_t rlim_cur;  /* Soft limit */
-	uint64_t rlim_max;  /* Hard limit (ceiling for rlim_cur) */
+	rlim_t rlim_cur;  /* Soft limit */
+	rlim_t rlim_max;  /* Hard limit (ceiling for rlim_cur) */
 };
 
 #endif
