86aa76e088521c768143755c113252a274f2f706
Balazs Gerofi <bgerofi@riken.jp>
2017-02-11 19:26:44 +0900

    IKC: increase ikc2linux channels' queue size

--- kernel/host.c
+++ kernel/host.c
@@ -715,7 +715,7 @@ void init_host_ikc2linux(int linux_cpu)
 		param.port = 503;
 		param.intr_cpu = linux_cpu;
 		param.pkt_size = sizeof(struct ikc_scd_packet);
-		param.queue_size = PAGE_SIZE * 4;
+		param.queue_size = 2 * num_processors * sizeof(struct ikc_scd_packet);
 		param.magic = 0x1129;
 		param.handler = dummy_packet_handler;
 
