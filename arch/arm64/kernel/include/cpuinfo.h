75f899826204183a48b49a33e8ad6dd9afe46791
fj1520as@aa.jp.fujitsu.com <fj1520as@aa.jp.fujitsu.com>
2017-04-07 16:23:14 +0900

    prepare: POSTK_DEBUG_ARCH_DEP_65

--- arch/arm64/kernel/include/cpuinfo.h
+++ arch/arm64/kernel/include/cpuinfo.h
@@ -11,17 +11,4 @@ struct cpuinfo_arm64 {
 	unsigned int hwid;	/* McKernel Original. */
 };
 
-/* @ref.impl arch/arm64/include/uapi/asm/hwcap.h */
-/*
- * HWCAP flags - for elf_hwcap (in kernel) and AT_HWCAP
- */
-#define HWCAP_FP		(1 << 0)
-#define HWCAP_ASIMD		(1 << 1)
-#define HWCAP_EVTSTRM		(1 << 2)
-#define HWCAP_AES		(1 << 3)
-#define HWCAP_PMULL		(1 << 4)
-#define HWCAP_SHA1		(1 << 5)
-#define HWCAP_SHA2		(1 << 6)
-#define HWCAP_CRC32		(1 << 7)
-
 #endif /* !__HEADER_ARM64_COMMON_CPUINFO_H */
