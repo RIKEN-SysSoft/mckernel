7c49fb136dcf5b2526601fe2b07c1cb063c4b8d6
fj1520as@aa.jp.fujitsu.com <toida@beluga.xtc-ext.css.fujitsu.com>
2017-06-01 12:05:14 +0900

    fixed : issues pointed out in review.(get|set_ip)

--- arch/arm64/kernel/include/ihk/context.h
+++ arch/arm64/kernel/include/ihk/context.h
@@ -32,14 +32,9 @@ struct pt_regs {
 
 typedef struct pt_regs ihk_mc_user_context_t;
 
-/* @ref.impl include/asm-generic/ptrace.h */
-/* Helpers for working with the instruction pointer */
-#ifndef GET_IP
-#define GET_IP(regs) ((regs)->pc)
-#endif
-#ifndef SET_IP
-#define SET_IP(regs, val) (GET_IP(regs) = (val))
-#endif
+/* @ref.impl arch/arm64/include/asm/ptrace.h */
+#define GET_IP(regs)		((unsigned long)(regs)->pc)
+#define SET_IP(regs, value)	((regs)->pc = ((uint64_t) (value)))
 
 /* @ref.impl include/asm-generic/ptrace.h */
 static inline unsigned long instruction_pointer(struct pt_regs *regs)
