77bb3038d360e842a94201930fb69124dce33f9e
Hannes Weisbach <hannes.weisbach@riken.jp>
2017-09-13 10:29:52 +0900

    Add PT_ENTRIES macro

--- arch/arm64/kernel/include/arch-memory.h
+++ arch/arm64/kernel/include/arch-memory.h
@@ -263,6 +263,8 @@ static const unsigned int  PTL1_ENTRIES = __PTL1_ENTRIES;
 
 #define PTE_FILEOFF		PTE_SPECIAL
 
+#define PT_ENTRIES		(PAGE_SIZE >> 3)
+
 #ifndef __ASSEMBLY__
 
 #include <ihk/types.h>
