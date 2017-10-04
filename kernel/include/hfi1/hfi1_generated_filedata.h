d7825240b24ce279476132ad23640deaca3a0092
Dominique Martinet <dominique.martinet@cea.fr>
2017-10-04 15:17:35 +0900

    hfi1 generated headers: add missing filedata file

---
+++ kernel/include/hfi1/hfi1_generated_filedata.h
@@ -0,0 +1,37 @@
+struct hfi1_filedata {
+	union {
+		char whole_struct[104];
+		struct {
+			char padding0[0];
+			struct hfi1_devdata *dd;
+		};
+		struct {
+			char padding1[8];
+			struct hfi1_ctxtdata *uctxt;
+		};
+		struct {
+			char padding2[16];
+			struct hfi1_user_sdma_comp_q *cq;
+		};
+		struct {
+			char padding3[24];
+			struct hfi1_user_sdma_pkt_q *pq;
+		};
+		struct {
+			char padding4[32];
+			u16 subctxt;
+		};
+		struct {
+			char padding5[56];
+			struct tid_rb_node **entry_to_rb;
+		};
+		struct {
+			char padding6[64];
+			spinlock_t tid_lock;
+		};
+		struct {
+			char padding7[72];
+			u32 tid_used;
+		};
+	};
+};
