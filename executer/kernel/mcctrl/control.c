5a65aaf9e43645d49aabf472b7093ae6a9ae39e9
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-02-20 11:20:21 +0900

    uti: Lock per_thread_data_hash_lock in mcctrl_put_per_proc_data()

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -1197,6 +1197,7 @@ int mcctrl_put_per_proc_data(struct mcctrl_per_proc_data *ppd)
 		struct mcctrl_per_thread_data *ptd;
 		struct mcctrl_per_thread_data *next;
 
+		write_lock_irqsave(&ppd->per_thread_data_hash_lock[i], flags);
 		list_for_each_entry_safe(ptd, next,
 		                         ppd->per_thread_data_hash + i, hash) {
 			packet = ptd->data;
@@ -1218,6 +1219,7 @@ int mcctrl_put_per_proc_data(struct mcctrl_per_proc_data *ppd)
 					 ppd->ud->ikc2linux[0]));
 #endif
 		}
+		write_unlock_irqrestore(&ppd->per_thread_data_hash_lock[i], flags);
 	}
 
 	flags = ihk_ikc_spinlock_lock(&ppd->wq_list_lock);
