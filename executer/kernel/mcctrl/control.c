441aeb32e25c3599e66ffeeb0c32c805c4821976
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-02-22 19:17:21 +0900

    uti: Fix clean-up of uti thread

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -2541,37 +2541,35 @@ mcexec_terminate_thread_unsafe(ihk_os_t os, int pid, int tid, long sig, struct t
 
 	printk("%s: target pid=%d,tid=%d,sig=%lx,task=%p\n", __FUNCTION__, pid, tid, sig, tsk);
 
+    write_lock_irqsave(&host_thread_lock, flags);
 	list_for_each_entry(thread, &host_threads, list) {
-		if(thread->pid == pid && thread->tid == tid) {
+		if(thread->tid == tid) {
 			break;
 		}
 	}
-
 	if (!thread) {
 		printk("%s: thread not found in host_threads list\n", __FUNCTION__);
 		write_unlock_irqrestore(&host_thread_lock, flags);
 		return -ESRCH;
 	}
 
-	printk("%s: thread=%p\n", __FUNCTION__, thread);
-
 	ppd = mcctrl_get_per_proc_data(usrdata, pid);
 	if (!ppd) {
 		kprintf("%s: ERROR: no per-process structure for PID %d??\n",
-		        __FUNCTION__, pid);
-		goto err;
+				__FUNCTION__, pid);
+		goto no_ppd;
 	}
 	packet = (struct ikc_scd_packet *)mcctrl_get_per_thread_data(ppd, tsk);
 	if (!packet) {
 		kprintf("%s: ERROR: no packet registered for TID %d\n",
-		       __FUNCTION__, tid);
-		goto err;
+				__FUNCTION__, tid);
+		goto no_ptd;
 	}
 
 	printk("%s: calling mcctrl_delete_per_thread_data,ppd=%p,tsk=%p\n", __FUNCTION__, ppd, tsk);
 	if ((rc = mcctrl_delete_per_thread_data(ppd, tsk))) {
 		kprintf("%s: ERROR: mcctrl_delete_per_thread_data failed (%d)\n", __FUNCTION__, rc);
-		goto err;
+		goto no_ptd;
 	}
 
 	printk("%s: calling __return_syscall, target pid=%d,tid=%d,sig=%lx,ppd->refcount=%d\n", __FUNCTION__, pid, tid, sig, atomic_read(&ppd->refcount));
@@ -2583,24 +2581,24 @@ mcexec_terminate_thread_unsafe(ihk_os_t os, int pid, int tid, long sig, struct t
 						   (usrdata->ikc2linux[smp_processor_id()] ?
 							usrdata->ikc2linux[smp_processor_id()] :
 							usrdata->ikc2linux[0]));
-
-	/* See the comment in mcexec_util_thread2 on how ppd->refcount reaches zero */
+ no_ptd:
 	printk("%s: ppd->refcount=%d\n", __FUNCTION__, atomic_read(&ppd->refcount));
 	if ((rc = mcctrl_put_per_proc_data(ppd)) <= 0) {
 		printk("%s: mcctrl_put_per_proc_data failed,rc=%d\n", __FUNCTION__, rc);
 	}
-err:
-	if(ppd) {
-		printk("%s: ppd->refcount=%d\n", __FUNCTION__, atomic_read(&ppd->refcount));
-		if ((rc = mcctrl_put_per_proc_data(ppd)) < 0) {
-			printk("%s: mcctrl_put_per_proc_data failed,rc=%d\n", __FUNCTION__, rc);
-		}
+
+	/* See the comment in mcexec_util_thread2 on how ppd->refcount reaches zero */
+	printk("%s: ppd->refcount=%d\n", __FUNCTION__, atomic_read(&ppd->refcount));
+	if ((rc = mcctrl_put_per_proc_data(ppd)) < 0) {
+		printk("%s: mcctrl_put_per_proc_data failed,rc=%d\n", __FUNCTION__, rc);
 	}
 
+ no_ppd:
 #if 0 /* debug */
 	list_del(&thread->list);
 	kfree(thread);
 #endif
+	write_unlock_irqrestore(&host_thread_lock, flags);
 
 	return 0;
 }
