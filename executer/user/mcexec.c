16b8fd36e5f8b74234c853b0d40331577e031b66
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-02-22 17:42:28 +0900

    uti: Add/Remove ifdefs for debug

--- executer/user/mcexec.c
+++ executer/user/mcexec.c
@@ -3161,16 +3161,8 @@ create_tracer(unsigned long user_start, unsigned long user_end)
 			case __NR_set_robust_list:/* debug */
 				break;
 			    case __NR_exit_group:
-#if 0 /* debug */
-				set_syscall_number(&args, -1);
-				set_syscall_args(uti_desc->tid, &args);
-#endif
 				exited++;
 			    case __NR_exit:
-#if 0 /* debug */
-				set_syscall_number(&args, -1);
-				set_syscall_args(uti_desc->tid, &args);
-#endif
 				exited++;
 				continue;
 			    case __NR_clone:
