0f2b0fcd33d4fa002934d41c81cb497c8b090882
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-03-01 18:51:49 +0900

    uti: Allow tracer to call release_handler() for the main process

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -376,7 +376,7 @@ static long mcexec_debug_log(ihk_os_t os, unsigned long arg)
 	return 0;
 }
 
-int mcexec_close_exec(ihk_os_t os);
+int mcexec_close_exec(ihk_os_t os, int pid);
 int mcexec_destroy_per_process_data(ihk_os_t os, int pid);
 
 static void release_handler(ihk_os_t os, void *param)
@@ -399,12 +399,13 @@ static void release_handler(ihk_os_t os, void *param)
 	}
 	write_unlock_irqrestore(&host_thread_lock, flags);
 
-	mcexec_close_exec(os);
+	mcexec_close_exec(os, info->pid);
 
 	/* Note that it will call return_syscall() */
-	mcexec_destroy_per_process_data(os, info->pid);
-
-	memset(&isp, '\0', sizeof isp);
+	if ((rc = mcexec_destroy_per_process_data(os, info->pid))) {
+		printk("%s: ERROR: mcexec_destroy_per_process_data failed,rc=%d\n", __FUNCTION__, rc);
+	}
+	memset(&isp, '\0', sizeof(isp));
 	isp.msg = SCD_MSG_CLEANUP_PROCESS;
 	isp.pid = info->pid;
 
@@ -1843,6 +1844,7 @@ int mcexec_destroy_per_process_data(ihk_os_t os, int pid)
 	struct mcctrl_per_proc_data *ppd = NULL;
 
 	ppd = mcctrl_get_per_proc_data(usrdata, pid);
+    dprintk("%s: pid=%d,tid=%d\n", __FUNCTION__, pid, task_pid_vnr(current));
 
 	if (ppd) {
 		/* One for the reference and one for deallocation.
@@ -1969,7 +1971,8 @@ int mcexec_close_exec(ihk_os_t os)
 		
 	down(&mckernel_exec_file_lock);
 	list_for_each_entry(mcef, &mckernel_exec_files, list) {
-		if (mcef->os == os && mcef->pid == task_tgid_vnr(current)) {
+		if (mcef->os == os && mcef->pid == pid) {
+			dprintk("%s: exec file found,%p,%p\n", __FUNCTION__, mcef, mcef->fp);
 			allow_write_access(mcef->fp);
 			fput(mcef->fp);
 			list_del(&mcef->list);
@@ -3157,7 +3160,7 @@ long __mcctrl_control(ihk_os_t os, unsigned int req, unsigned long arg,
 		return mcexec_open_exec(os, (char *)arg);
 
 	case MCEXEC_UP_CLOSE_EXEC:
-		return mcexec_close_exec(os);
+		return mcexec_close_exec(os, task_tgid_vnr(current));
 
 	case MCEXEC_UP_PREPARE_DMA:
 		return mcexec_pin_region(os, (unsigned long *)arg);
