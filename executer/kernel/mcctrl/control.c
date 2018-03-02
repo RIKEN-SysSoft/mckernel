e78063c5129d9e1a67d8a06597914fbf35fd0c05
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-03-02 17:04:40 +0900

    uti: Add debug messages/comments

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -2519,23 +2519,19 @@ mcexec_util_thread2(ihk_os_t os, unsigned long arg, struct file *file)
 	write_lock_irqsave(&host_thread_lock, flags);
 	list_add_tail(&thread->list, &host_threads);
 	write_unlock_irqrestore(&host_thread_lock, flags);
-#if 1
-	/* How ppd refcount reaches zero depends on how utility-thread exits:
-         MCEXEC_UP_CREATE_PPD: set to 1
-         mcexec_util_thread2: inc to 2
 
-  	     tracer alive:
-		   create_tracer()
-		     tracer detects exit/exit_group/killed by signal
-               mcexec_terminate_thread: dec to 1
-	       release_handler(): dec to 0
+	/* How ppd refcount reaches zero depends on how utility-thread exits:
+         (1) MCEXEC_UP_CREATE_PPD: set to 1
+         (2) mcexec_util_thread2: inc to 2
+		 (3) tracer detects exit/exit_group/killed by signal
+             and calls mcexec_terminate_thread() to dec to 1
+	     (4) release_handler(): dec to 0
 
 	     KNOWN ISSUE: 
-           mcexec_terminate_thread() isn't called when tracer is dead
-	       so the refcount is 1 when exiting release_handler()
+           refcount remains 1 when tracer failed to call mcexec_terminate_thread()
 	*/
 	ppd = mcctrl_get_per_proc_data(usrdata, task_tgid_vnr(current));
-#endif
+
 	return 0;
 }
 
