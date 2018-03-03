232b18c849fe6eaf7440e7596158227196af4264
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-03-03 17:04:21 +0900

    pager_req_release(): Correct debug messages

--- executer/kernel/mcctrl/syscall.c
+++ executer/kernel/mcctrl/syscall.c
@@ -1412,7 +1412,7 @@ static int pager_req_release(ihk_os_t os, uintptr_t handle, int unref)
 
 	error = down_interruptible(&pager_sem);
 	if (error) {
-		printk("pager_req_relase(%p,%lx,%d):signaled. %d\n", os, handle, unref, error);
+		printk("pager_req_release(%p,%lx,%d):signaled. %d\n", os, handle, unref, error);
 		goto out;
 	}
 
@@ -1432,7 +1432,7 @@ static int pager_req_release(ihk_os_t os, uintptr_t handle, int unref)
 	up(&pager_sem);
 
 	if (error) {
-		printk("pager_req_relase(%p,%lx,%d):pager not found. %d\n", os, handle, unref, error);
+		printk("pager_req_release(%p,%lx,%d):pager not found. %d\n", os, handle, unref, error);
 		goto out;
 	}
 
@@ -1448,7 +1448,7 @@ static int pager_req_release(ihk_os_t os, uintptr_t handle, int unref)
 
 	error = 0;
 out:
-	dprintk("pager_req_relase(%p,%lx,%d): %d\n", os, handle, unref, error);
+	dprintk("pager_req_release(%p,%lx,%d): %d\n", os, handle, unref, error);
 	return error;
 }
 
