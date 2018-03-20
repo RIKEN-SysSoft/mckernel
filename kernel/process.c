8a007c6bbd498f91f48b66bac68f69eb0b5c092c
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-03-20 14:05:19 +0900

    Fix deadlock involving mmap_sem and memory_range_lock
    squash me into the old commit.

--- kernel/process.c
+++ kernel/process.c
@@ -2003,9 +2003,18 @@ static int do_page_fault_process_vm(struct process_vm *vm, void *fault_addr0, ui
 	
 #if 1
 	if (thread->vm->memory_range_lock_writer_count == 0) {
-		ihk_mc_spinlock_lock_noirq(&vm->memory_range_lock);
-		locked = 1;
+		/* For the case where memory_range_lock_writer_count is incremented after memory_range_lock is taken. */
+		while (1) {
+			if (thread->vm->memory_range_lock_writer_count > 0) {
+				goto skip;
+			}
+			if (ihk_mc_spinlock_trylock_noirq(&vm->memory_range_lock)) {
+				locked = 1;
+				break;
+			}
+		}
 	} else {
+		skip:
 		kprintf("%s: INFO: skip locking of memory_range_lock,pid=%d,tid=%d\n", __FUNCTION__, thread->proc->pid, thread->tid);
 	}	
 #else
