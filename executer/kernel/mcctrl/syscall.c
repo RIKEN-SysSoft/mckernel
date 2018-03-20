4ff8b110876551d7dbc0a513c7964d02b04263e3
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-03-20 14:02:51 +0900

    rus_vm_fault: Return VM_FAULT_SIGBUS when per-process data is not found

--- executer/kernel/mcctrl/syscall.c
+++ executer/kernel/mcctrl/syscall.c
@@ -934,7 +934,8 @@ static int rus_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 	if (!ppd) {
 		kprintf("%s: ERROR: no per-process structure for PID %d??\n", 
 				__FUNCTION__, task_tgid_vnr(current));
-		return -EINVAL;
+		ret = VM_FAULT_SIGBUS;
+		goto no_ppd;
 	}
 
 	ptd = mcctrl_get_per_thread_data(ppd, current);
@@ -1157,6 +1158,7 @@ put_and_out:
 	pr_ptd("put", task_pid_vnr(current), ptd);
  no_ptd:
 	mcctrl_put_per_proc_data(ppd);
+ no_ppd:
 	return ret;
 }
 
