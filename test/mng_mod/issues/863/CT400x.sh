309da8fc530ba79a46de6c209d86e59d46727ca3
Tomoki Shirasawa <tomoki.shirasawa.kk@hitachi-solutions.com>
2018-03-23 14:48:18 +0900

    issue/863: add 8 testcases

---
+++ test/mng_mod/issues/863/CT400x.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+MCKERNEL_DIR=/home/shirasawa/wallaby11-smp-x86/development/mic
+export PATH=$MCKERNEL_DIR/bin:$PATH
+
+./CT4001.sh
+./CT4002.sh
+./CT4003.sh
+./CT4004.sh
+./CT4005.sh
+./CT4006.sh
+./CT4007.sh
+./CT4008.sh
