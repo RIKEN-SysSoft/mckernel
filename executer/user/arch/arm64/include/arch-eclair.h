ba5ff88d30ba22df60ecb78deb70047ccab0140e
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-06-06 11:49:19 +0900

    Fix bug#3-5 Add 64KiB-pagesize mckernel-mapping definition to eclair.(default:4KiB,VABITS=48)

--- executer/user/arch/arm64/include/arch-eclair.h
+++ executer/user/arch/arm64/include/arch-eclair.h
@@ -1,11 +1,51 @@
-/* arch-eclair.h COPYRIGHT FUJITSU LIMITED 2016 */
+/* arch-eclair.h COPYRIGHT FUJITSU LIMITED 2016-2018 */
 #ifndef HEADER_USER_ARM64_ECLAIR_H
 #define HEADER_USER_ARM64_ECLAIR_H
 
-/* VA_BITS=48, 4K_PAGE address */
-#define MAP_KERNEL	0xffffffffff800000
-#define MAP_ST		0xffff800000000000
-#define MAP_KERNEL_TEXT	"0xffffffffff800000"
+#define CONFIG_ARM64_VA_BITS	48
+//#define CONFIG_ARM64_64K_PAGES
+
+#ifdef CONFIG_ARM64_64K_PAGES
+#
+# if (CONFIG_ARM64_VA_BITS == 42)
+#  /* VA_BITS=42, 64K_PAGE address */
+#  define MAP_KERNEL		0xffffffffe0000000
+#  define MAP_ST		0xfffffe0000000000
+#  define MAP_KERNEL_TEXT	"0xffffffffe0000000"
+#
+# elif (CONFIG_ARM64_VA_BITS == 48)
+#  /* VA_BITS=48, 64K_PAGE address */
+#  define MAP_KERNEL		0xffffffffe0000000
+#  define MAP_ST		0xffff800000000000
+#  define MAP_KERNEL_TEXT	"0xffffffffe0000000"
+#
+# else
+#
+#  error "No support VA_BITS and PAGE_SIZE"
+#
+# endif
+#
+#else /* CONFIG_ARM64_64K_PAGES */
+#
+# if (CONFIG_ARM64_VA_BITS == 39)
+#  /* VA_BITS=39, 4K_PAGE address */
+#  define MAP_KERNEL		0xffffffffff800000
+#  define MAP_ST		0xffffffc000000000
+#  define MAP_KERNEL_TEXT	"0xffffffffff800000"
+#
+# elif (CONFIG_ARM64_VA_BITS == 48)
+#  /* VA_BITS=48, 4K_PAGE address */
+#  define MAP_KERNEL		0xffffffffff800000
+#  define MAP_ST		0xffff800000000000
+#  define MAP_KERNEL_TEXT	"0xffffffffff800000"
+#
+# else
+#
+#  error "No support VA_BITS and PAGE_SIZE"
+#
+# endif
+#
+#endif /* CONFIG_ARM64_64K_PAGES */
 
 #define ARCH_CLV_SPAN	"arm64_cpu_local_variables_span"
 
