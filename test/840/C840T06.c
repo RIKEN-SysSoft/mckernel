6602cf442c1652402807d7bc971a5ebde4170455
Tomoki Shirasawa <tomoki.shirasawa.kk@hitachi-solutions.com>
2018-06-07 10:04:33 +0900

    add test cases

---
+++ test/840/C840T06.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
+
+int
+main(int argc, char **argv)
+{
+	for(;;);
+	exit(0);
+}
