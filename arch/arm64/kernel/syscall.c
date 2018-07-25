2656ce5d66e548bf26fed7c78820cf94c861aa7e
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-07-25 19:08:42 +0900

    uti: Fix dead-lock of calling terminate() from terminate()
    Conflicts:
        arch/x86_64/kernel/syscall.c
        kernel/syscall.c
    
    original commit-id: 3b277b2354f7dc40596352cbd0d10f85e39b1910

--- arch/arm64/kernel/syscall.c
+++ arch/arm64/kernel/syscall.c
@@ -1595,7 +1595,9 @@ done:
 	}
 
 	if (tthread->thread_offloaded) {
-		interrupt_syscall(tthread, sig);
+		if (!tthread->proc->nohost) {
+			interrupt_syscall(tthread, sig);
+		}
 		release_thread(tthread);
 		return 0;
 	}
