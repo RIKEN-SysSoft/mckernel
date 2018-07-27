b1aa94d417bbcedf4d8f4dee3954f9693a5665f1
Dominique Martinet <dominique.martinet@cea.fr>
2018-07-27 14:30:46 +0900

    arm64 arch-perfctr.h: remove duplicate enums
    Some enums were redefined in lib/include/mc_perf_event.h in commit
    1284060 ("support PERF_TYPE_{HARDWARE|HWCACHE} in perf_event_open")
    
    Change-Id: I1a98699955ca7fd6135b2a7dde72ed4df77b1974

--- arch/arm64/kernel/include/arch-perfctr.h
+++ arch/arm64/kernel/include/arch-perfctr.h
@@ -35,38 +35,4 @@ void arm64_disable_pmu(void);
 int armv8pmu_init(struct arm_pmu* cpu_pmu);
 
 /* TODO[PMU]: 共通部に定義があっても良い。今後の動向を見てここの定義を削除する */
-/*
- * Generalized hardware cache events:
- *
- *       { L1-D, L1-I, LLC, ITLB, DTLB, BPU, NODE } x
- *       { read, write, prefetch } x
- *       { accesses, misses }
- */
-enum perf_hw_cache_id {
-	PERF_COUNT_HW_CACHE_L1D			= 0,
-	PERF_COUNT_HW_CACHE_L1I			= 1,
-	PERF_COUNT_HW_CACHE_LL			= 2,
-	PERF_COUNT_HW_CACHE_DTLB		= 3,
-	PERF_COUNT_HW_CACHE_ITLB		= 4,
-	PERF_COUNT_HW_CACHE_BPU			= 5,
-	PERF_COUNT_HW_CACHE_NODE		= 6,
-
-	PERF_COUNT_HW_CACHE_MAX,		/* non-ABI */
-};
-
-enum perf_hw_cache_op_id {
-	PERF_COUNT_HW_CACHE_OP_READ		= 0,
-	PERF_COUNT_HW_CACHE_OP_WRITE		= 1,
-	PERF_COUNT_HW_CACHE_OP_PREFETCH		= 2,
-
-	PERF_COUNT_HW_CACHE_OP_MAX,		/* non-ABI */
-};
-
-enum perf_hw_cache_op_result_id {
-	PERF_COUNT_HW_CACHE_RESULT_ACCESS	= 0,
-	PERF_COUNT_HW_CACHE_RESULT_MISS		= 1,
-
-	PERF_COUNT_HW_CACHE_RESULT_MAX,		/* non-ABI */
-};
-
 #endif
