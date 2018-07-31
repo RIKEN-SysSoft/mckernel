b0e427cab806970d15661be64c8a0af352f4c640
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-07-31 18:51:18 +0900

    x86 futex: fix out of bounds shift
    8 << 28 needs unsigned to fit, other shifts were done to truncate
    the input, use a mask instead
    
    Change-Id: I81ba41595f4629f1df554e34392116440ff3b641
    
    original commit-id: bc887aab44c4818c5f28250304d953cdad63d160

--- arch/x86_64/kernel/include/arch-futex.h
+++ arch/x86_64/kernel/include/arch-futex.h
@@ -64,12 +64,13 @@ static inline int futex_atomic_cmpxchg_inatomic(int __user *uaddr, int oldval,
 	return oldval;
 }
 
-static inline int futex_atomic_op_inuser(int encoded_op, int __user *uaddr)
+static inline int futex_atomic_op_inuser(int encoded_op,
+					 int __user *uaddr)
 {
 	int op = (encoded_op >> 28) & 7;
 	int cmp = (encoded_op >> 24) & 15;
-	int oparg = (encoded_op << 8) >> 20;
-	int cmparg = (encoded_op << 20) >> 20;
+	int oparg = (encoded_op & 0x00fff000) >> 12;
+	int cmparg = encoded_op & 0xfff;
 	int oldval = 0, ret, tem;
 
 	if (encoded_op & (FUTEX_OP_OPARG_SHIFT << 28))
