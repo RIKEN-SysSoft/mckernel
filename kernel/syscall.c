0c0b574ba45adc95cda142aade98d163ec9fd4c7
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-08-07 10:22:13 +0900

    Fix bug#3-15, interrupt_syscall returned -ERESTARTSYS mcexec hungup. (POSTK_TEMP_FIX_70)

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -463,7 +463,11 @@ long do_syscall(struct syscall_request *req, int cpu, int pid)
 
 	/* -ERESTARTSYS indicates that the proxy process is gone
 	 * and the application should be terminated */
+#ifdef POSTK_DEBUG_TEMP_FIX_70 /* interrupt_syscall returned -ERESTARTSYS fix */
+	if (rc == -ERESTARTSYS && req->number != __NR_kill) {
+#else /* POSTK_DEBUG_TEMP_FIX_70 */
 	if (rc == -ERESTARTSYS) {
+#endif /* POSTK_DEBUG_TEMP_FIX_70 */
 		kprintf("%s: proxy PID %d is dead, terminate()\n",
 			__FUNCTION__, thread->proc->pid);
 		thread->proc->nohost = 1;
