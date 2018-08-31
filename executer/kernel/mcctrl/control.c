91b3a88cccd12f53f5ac6258cbe6ceb247547d3e
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-09-01 00:18:40 +0900

    uti: Suppress debug messages in control.c
    Change-Id: Ia444da234dbfd4a13e99a8b7d5d7f4556b17532e

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -404,13 +404,13 @@ static void release_handler(ihk_os_t os, void *param)
 	isp.msg = SCD_MSG_CLEANUP_PROCESS;
 	isp.pid = info->pid;
 
-	printk("%s: SCD_MSG_CLEANUP_PROCESS, info: %p, cpu: %d\n", __FUNCTION__, info, info->cpu);
+	dprintk("%s: SCD_MSG_CLEANUP_PROCESS, info: %p, cpu: %d\n", __FUNCTION__, info, info->cpu);
 	mcctrl_ikc_send(os, info->cpu, &isp);
 	if (os_ind >= 0) {
 		printk("%s: calling delete_pid_entry,os_ind=%d,pid=%d\n", __FUNCTION__, os_ind, info->pid);
 		delete_pid_entry(os_ind, info->pid);
 	}
-	printk("%s: calling kfree,param=%p,info->pid=%d\n", __FUNCTION__, param, info->pid);
+
 	kfree(param);
 	dprintk("%s: SCD_MSG_CLEANUP_PROCESS, info: %p OK\n",
 			__FUNCTION__, info);
@@ -1214,7 +1214,7 @@ int mcexec_syscall(struct mcctrl_usrdata *ud, struct ikc_scd_packet *packet)
 	ppd = mcctrl_get_per_proc_data(ud, pid);
 
 	if (unlikely(!ppd)) {
-		kprintf("%s: INFO: no per-process structure for PID %d,syscall nr=%lu,rtid=%d\n", __FUNCTION__, pid, packet->req.number, packet->req.rtid);
+		dprintk("%s: INFO: no per-process structure for PID %d,syscall nr=%lu,rtid=%d\n", __FUNCTION__, pid, packet->req.number, packet->req.rtid);
 
 		/* We use ERESTARTSYS to tell the LWK that the proxy
 		 * process is gone and the application should be terminated */
