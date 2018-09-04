752fce254549ab8da53f500b72d9692df453acde
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-09-04 10:34:19 +0900

    uti: do_syscall: Don't warn when proxy is gone
    This is because this is a normal case since terminate() is changed so
    that it first kills all mcexec threads and then kill McKernel threads.
    
    Change-Id: I88380bf28b60645d361baded525d71105235c16f

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -483,7 +483,7 @@ long do_syscall(struct syscall_request *req, int cpu, int pid)
 	/* -ERESTARTSYS indicates that the proxy process is gone
 	 * and the application should be terminated */
 	if (rc == -ERESTARTSYS) {
-		kprintf("%s: proxy PID %d is dead, terminate()\n",
+		dkprintf("%s: proxy PID %d is dead, terminate()\n",
 			__FUNCTION__, thread->proc->pid);
 		thread->proc->nohost = 1;
 	}
