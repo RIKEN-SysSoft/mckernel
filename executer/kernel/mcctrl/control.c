7fe8b5f1396022401e7bdecf492a706e0b0475c0
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-09-11 16:42:34 +0900

    uti: Cosmetic change
    original commit-id: b9c0cdddabf2bb6f1cf46a9b48ab5a2ccffc448d

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -2635,7 +2635,6 @@ mcexec_sig_thread(ihk_os_t os, unsigned long arg, struct file *file)
 static long
 mcexec_terminate_thread_unsafe(ihk_os_t os, int pid, int tid, long sig, struct task_struct *tsk)
 {
-	int rc;
 	struct ikc_scd_packet *packet;
 	struct mcctrl_usrdata *usrdata = ihk_host_os_get_usrdata(os);
 	struct mcctrl_per_proc_data *ppd;
