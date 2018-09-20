6cbcf9aae92f5f2ac9942c1c516a62d0ba120865
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-09-20 14:27:26 +0900

    execve: Fix calling ptrace_report_signal after preemption is disabled
    Change-Id: I451d28d985ab330d855501597713e982b8febf4e
    Refs: 1194
    
    original commit-id: 2910818f06d2b25eb24a60f1d874d2a93ed281af

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1984,7 +1984,9 @@ static int ptrace_report_exec(struct thread *thread)
 		int sig = (SIGTRAP | (PTRACE_EVENT_EXEC << 8));
 
 		memcpy(&ctx, &thread->ctx, sizeof ctx);
+		preempt_enable();
 		ptrace_report_signal(thread, sig);
+		preempt_disable();
 		memcpy(&thread->ctx, &ctx, sizeof ctx);
 	}
 	return 0;
