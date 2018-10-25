fb4dde8fce6581b1307f441baac09995787dc2b2
Dominique Martinet <dominique.martinet@cea.fr>
2018-10-25 10:18:55 +0900

    mcctrl rus_mmap: make vma->vm_flags arch-dependent
    Change-Id: I5ec89b3ff80af6bf0ede342eb5816df8c78de348
    [Dominique: renamed arch_vm_flags to arch_rus_vm_flags]
    Fujitsu: POSTK_DEBUG_ARCH_DEP_100

--- executer/kernel/mcctrl/arch/arm64/include/archdeps.h
+++ executer/kernel/mcctrl/arch/arm64/include/archdeps.h
@@ -17,5 +17,11 @@ static inline bool pte_is_write_combined(pte_t pte)
 
 #define ARMV8_IDX_COUNTER0	1
 #define ARCH_PERF_CONTER_START	ARMV8_IDX_COUNTER0
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,7,0)
+static const unsigned long arch_rus_vm_flags = VM_RESERVED | VM_MIXEDMAP | VM_EXEC;
+#else
+static const unsigned long arch_rus_vm_flags = VM_DONTDUMP | VM_MIXEDMAP | VM_EXEC;
+#endif
 #endif /* __HEADER_MCCTRL_ARM64_ARCHDEPS_H */
 #endif /* POSTK_DEBUG_ARCH_DEP_83 */
