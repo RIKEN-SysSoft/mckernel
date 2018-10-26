c760e54a56181cade65a5dc43b91dc436c603719
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-10-26 15:51:53 +0900

    open, xpmem_open: Split/Move to architecture dependent directories
    In arm64, opening /dev/xpmem is hooked in sys_openat, but not in
    sys_open. This commit move sys_open to x86_64 directory and splits
    xpmem_open into xpmem_open and xpmem_openat and puts the former to the
    directory as well.
    
    Change-Id: I3b4f7bf0e152c359250bb2b56910db9192390cb1
    Fujitsu: POSTK_DEBUG_ARCH_DEP_46, POSTK_DEBUG_ARCH_DEP_62

--- kernel/include/xpmem.h
+++ kernel/include/xpmem.h
@@ -19,14 +19,13 @@
 
 #define XPMEM_DEV_PATH  "/dev/xpmem"
 
-#if defined(POSTK_DEBUG_ARCH_DEP_46) || defined(POSTK_DEBUG_ARCH_DEP_62)
-extern int xpmem_open(int, const char*, int, ihk_mc_user_context_t *ctx);
-#else /* POSTK_DEBUG_ARCH_DEP_46 || POSTK_DEBUG_ARCH_DEP_62 */
-extern int xpmem_open(ihk_mc_user_context_t *ctx);
-#endif /* POSTK_DEBUG_ARCH_DEP_46 || POSTK_DEBUG_ARCH_DEP_62 */
-extern int xpmem_remove_process_memory_range(struct process_vm *vm, 
+int xpmem_open(const char *pathname,
+	       int flags, ihk_mc_user_context_t *ctx);
+int xpmem_openat(const char *pathname,
+		 int flags, ihk_mc_user_context_t *ctx);
+int xpmem_remove_process_memory_range(struct process_vm *vm, 
 	struct vm_range *vmr);
-extern int xpmem_fault_process_memory_range(struct process_vm *vm, 
+int xpmem_fault_process_memory_range(struct process_vm *vm, 
 	struct vm_range *vmr, unsigned long vaddr, uint64_t reason);
 
 #endif /* _XPMEM_H */
