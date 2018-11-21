009dcc3e8c60e94d0a8503a3dc6725af371ae80f
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-11-21 11:25:10 +0900

    Test "xpmem: support for fork()" on arm64
    Change-Id: I12c628312157f35e239d3c5e67fa38adf156406b
    Refs #925

---
+++ test/mng_mod/issues/925/arm64/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+## run.sh COPYRIGHT FUJITSU LIMITED 2018 ##
+
+USELTP=0
+USEOSTEST=0
+
+. ../../../../common.sh
+
+${MCEXEC} ./CT_001
+${MCEXEC} ./CT_002
+${MCEXEC} ./CT_003
+${MCEXEC} ./CT_004
+${MCEXEC} ./CT_005
+${MCEXEC} ./CT_006
+sleep 2
+${MCEXEC} ./CT_007
+${MCEXEC} ./CT_008
+${MCEXEC} ./CT_009
+${MCEXEC} ./CT_010
+${MCEXEC} ./CT_011
+
+${MCEXEC} ./rusage012
