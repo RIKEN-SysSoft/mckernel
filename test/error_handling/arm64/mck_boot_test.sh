b5f9581a494eb5d1cf2235490db7fc0abd1f34fd
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-12-04 17:09:33 +0900

    Test "Error handling improvement" on arm64
    The following test set:
      execve: fix memory leak
      add: NULL check for master_channel at IKC interrupt_handler.
      Fix the check routine for elf sections (Fujitsu: POSTK_TEMP_FIX_77)
    
    Change-Id: I16c2a341c48f6df10a4839be08b93ea16bda8fbe
    Refs: #727
    Refs: #873
    Refs: #1011

---
+++ test/error_handling/arm64/mck_boot_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+MCK_DIR=/opt/ppos
+REP_NUM=100
+
+for i in `seq 1 ${REP_NUM}`
+do
+	sudo ${MCK_DIR}/sbin/mcstop+release.sh
+	sleep 1
+	sudo ${MCK_DIR}/sbin/mcreboot.sh
+
+	if [ $? -ne 0 ]; then
+		echo "[NG] failed to boot Mckernel  :${i}"
+		exit 1
+	fi
+done
+
+echo "[OK] succeed to boot McKernel ${i} times"
