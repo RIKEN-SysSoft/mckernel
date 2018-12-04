eb090903cc73457b79dace3fa2837498172bdce5
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-12-04 17:09:33 +0900

    Test "Error handling improvement" on arm64
    The following test set:
      execve: fix memory leak
      add: NULL check for master_channel at IKC interrupt_handler.
      Fix the check routine for elf sections (Fujitsu: POSTK_TEMP_FIX_77)
    
    Change-Id: I16c2a341c48f6df10a4839be08b93ea16bda8fbe
    Refs: #727
    Refs: #873
    Refs: #1011

---
+++ test/error_handling/arm64/run.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+## run.sh COPYRIGHT FUJITSU LIMITED 2018 ##
+
+USELTP=1
+USEOSTEST=1
+
+. ../../common.sh
+
+result=0
+
+###########################
+# Issue727 test(no-patch) #
+###########################
+
+${MCEXEC} ./exec
+if [ $? == 0 ]; then
+	echo "Issue727_0001: OK"
+else
+	echo "Issue727_0001: NG"
+	result=-1
+fi
+
+##################
+# Issue 873 test #
+##################
+
+isseu873_result=0
+REP_NUM=100
+for i in `seq -f %03g 1 ${REP_NUM}`
+do
+	sudo "${SBIN}/mcstop+release.sh"
+	sleep 1
+	sudo "${SBIN}/mcreboot.sh" ${BOOTPARAM}
+
+	if [ $? != 0 ]; then
+		echo ""
+		echo "[NG] failed to boot Mckernel  :${i}"
+		isseu873_result=-1
+		break
+	fi
+	echo -en "Issue873_0001 n=${i}/${REP_NUM} ok.\r"
+done
+echo ""
+
+if [ ${isseu873_result} == 0 ]; then
+	echo "[OK] succeed to boot McKernel ${i} times"
+	echo "Issue873_0001: OK"
+else
+	echo "Issue873_0001: NG"
+	result=-1
+fi
+
+#############################
+# Issue 1011 test(no-patch) #
+#############################
+
+${MCEXEC} hostname
+if [ $? == 0 ]; then
+	echo "Issue1011_0001: OK"
+else
+	echo "Issue1011_0001: NG"
+	result=-1
+fi
+
+##################
+# ulimit -u test #
+##################
+
+${MCEXEC} ${TESTMCK} -s kill -n 1 -- -p 6
+if [ $? == 0 ]; then
+	echo "ulimit -u 0001: OK"
+else
+	echo "ulimit -u 0001: NG"
+	result=-1
+fi
+
+ulimit -u 9
+${MCEXEC} ${TESTMCK} -s kill -n 1 -- -p 6
+if [ $? != 0 ]; then
+	echo "ulimit -u 0002: OK"
+else
+	echo "ulimit -u 0002: NG"
+	result=-1
+fi
+
+#######################
+# LTP regression test #
+#######################
+
+export PATH=${LTPBIN}:${PATH}
+while read line
+do
+	tp=`echo ${line} | cut -d ' ' -f 1`
+
+	if [ ! -e ${LTPBIN}/${tp} ]; then
+		echo "${LTPBIN}/${tp} not found."
+		continue
+	fi
+
+	timeout -sKILL 5m ${MCEXEC} ${LTPBIN}/${line}
+	if [ $? != 0 ]; then
+		echo "##### ${tp} returned not 0 #####"
+		result=-1
+	fi
+done < ./ltplist.txt
+
+exit ${result}
