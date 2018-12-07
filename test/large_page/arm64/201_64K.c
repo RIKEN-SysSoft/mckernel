c79e7609b2df0656e416b8c28f6b2d2c91c03529
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-12-07 19:28:51 +0900

    Add test programs for lage page
    Tests arm64 specific, contiguous bit based large pages as well.
    
    Change-Id: I09edad8cfde6c23a259f1f32cfc97974d9cb63c3

---
+++ test/large_page/arm64/201_64K.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "../util.h"
+
+#define PAGE_SHIFT_2M 21
+#define PAGE_SIZE_2M (1UL << PAGE_SHIFT_2M)
+
+#define PAGE_SHIFT_512M 29
+#define PAGE_SIZE_512M (1UL << PAGE_SHIFT_512M)
+
+int trial_num;
+
+int shmat_thp(char **argv, size_t page_size)
+{
+	int ret;
+	key_t key;
+	int shmid;
+	struct shmid_ds shmid_ds;
+	char *addr_shmat;
+
+	key = ftok(argv[0], 0);
+
+	shmid = shmget(key, page_size, IPC_CREAT | 0660);
+	NG(shmid != -1, "shmget failed\n");
+
+	addr_shmat = shmat(shmid, NULL, 0);
+	NG(addr_shmat != (void *)-1, "shmat failed\n");
+
+	addr_shmat[0] = 'z';
+	printf("large page request, trial#: %03d, addr: %016lx, size: %ld\n",
+	       trial_num++, (unsigned long)addr_shmat, page_size);
+
+	ret = shmctl(shmid, IPC_RMID, &shmid_ds);
+	NG(ret != -1, "shmctl failed\n");
+
+	ret = shmdt(addr_shmat);
+	NG(ret != -1, "shmdt failed\n");
+
+	return 0;
+ fn_fail:
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	int ret;
+
+	ret = shmat_thp(argv, PAGE_SIZE_2M);
+	NG(ret == 0, "shmat_thp failed, size: %ld\n",
+	   PAGE_SIZE_2M);
+
+	ret = shmat_thp(argv, PAGE_SIZE_512M);
+	NG(ret == 0, "shmat_thp failed, size: %ld\n",
+	   PAGE_SIZE_512M);
+
+	return 0;
+ fn_fail:
+	return 1;
+}
---
+++ test/large_page/x86_64/502_4K.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include "../util.h"
+
+#define TARGET_PAGE_SHIFT 30
+#define TARGET_PAGE_SIZE (1UL << TARGET_PAGE_SHIFT)
+
+/* .data */
+char addr_data[TARGET_PAGE_SIZE] = { 1 };
+
+/* .bss */
+char addr_bss[TARGET_PAGE_SIZE];
+
+int main(int argc, char **argv)
+{
+	int trial_num = 0;
+	char addr_stack[TARGET_PAGE_SIZE];
+
+	/* .data */
+	addr_data[0] = 'z';
+	printf("large page request, trial#: %03d, addr: %016lx, size: %ld\n",
+	       trial_num++, (unsigned long)addr_data, TARGET_PAGE_SIZE);
+
+	/* .bss */
+	addr_bss[0] = 'z';
+	printf("large page request, trial#: %03d, addr: %016lx, size: %ld\n",
+	       trial_num++, (unsigned long)addr_bss, TARGET_PAGE_SIZE);
+
+	/* stack */
+	addr_stack[0] = 'z';
+	printf("large page request, trial#: %03d, addr: %016lx, size: %ld\n",
+	       trial_num++, (unsigned long)addr_stack, TARGET_PAGE_SIZE);
+
+	return 0;
+}
