cee2e807145912fd6baf078bd96ee5c02748de84
Masamichi Takagi <masamichi.takagi@riken.jp>
2018-12-13 17:13:27 +0900

    shmobj: Fix rusage report size
    Change-Id: I6fb1ff361bddb3fa64532bb81eba5ccf381d1a70

--- kernel/shmobj.c
+++ kernel/shmobj.c
@@ -271,7 +271,8 @@ static void shmobj_destroy(struct shmobj *obj)
 			/* Track change in page->count for shmobj.
 			   It is decremented in here or shmobj_invalidate() or clear_range(). */
 			dkprintf("%lx-,%s: calling memory_stat_rss_sub(),phys=%lx,size=%ld,pgsize=%ld\n", phys, __FUNCTION__, phys, npages * PAGE_SIZE, PAGE_SIZE);
-			memory_stat_rss_sub(npages * PAGE_SIZE, npages * PAGE_SIZE);
+			memory_stat_rss_sub(1UL << obj->pgshift,
+					    1UL << obj->pgshift);
 		}
 #if 0
 		dkprintf("shmobj_destroy(%p):"
@@ -459,7 +460,8 @@ static int shmobj_invalidate_page(struct memobj *memobj, uintptr_t phys,
 			/* Track change in page->count for shmobj. 
 			 It is decremented in here or shmobj_destroy() or clear_range(). */
 			dkprintf("%lx-,%s: calling memory_stat_rss_sub(),phys=%lx,size=%ld,pgsize=%ld\n", phys, __FUNCTION__, phys, pgsize, PAGE_SIZE);
-			memory_stat_rss_sub(pgsize, pgsize);
+			memory_stat_rss_sub(1UL << obj->pgshift,
+					    1UL << obj->pgshift);
 		}
 	}
 
