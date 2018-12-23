384491c2251334e6aead268f8ac943efa8430fc7
Balazs Gerofi <bgerofi@riken.jp>
2018-12-24 01:32:55 +0900

    move rusage into kernel ELF image (avoid dynamic alloc before NUMA init)
    Change-Id: I7fe86244c8707694b379e567b31de65ee2c56887

--- kernel/mem.c
+++ kernel/mem.c
@@ -1741,7 +1741,6 @@ void ihk_mc_clean_micpa(void){
 
 static void rusage_init()
 {
-	int npages;
 	unsigned long phys;
 	const struct ihk_mc_cpu_info *cpu_info = ihk_mc_get_cpu_info();
 
@@ -1749,15 +1748,13 @@ static void rusage_init()
 		panic("rusage_init: PANIC: ihk_mc_get_cpu_info returned NULL");
 	}
 
-	npages = (sizeof(struct rusage_global) + PAGE_SIZE -1) >> PAGE_SHIFT;
-	rusage = ihk_mc_alloc_pages(npages, IHK_MC_AP_CRITICAL);
-	memset(rusage, 0, npages * PAGE_SIZE);
-	rusage->num_processors = cpu_info->ncpus;
-	rusage->num_numa_nodes = ihk_mc_get_nr_numa_nodes();
-	rusage->ns_per_tsc = ihk_mc_get_ns_per_tsc();
-	phys = virt_to_phys(rusage);
+	memset(&rusage, 0, sizeof(rusage));
+	rusage.num_processors = cpu_info->ncpus;
+	rusage.num_numa_nodes = ihk_mc_get_nr_numa_nodes();
+	rusage.ns_per_tsc = ihk_mc_get_ns_per_tsc();
+	phys = virt_to_phys(&rusage);
 	ihk_set_rusage(phys, sizeof(struct rusage_global));
-	dkprintf("%s: rusage->total_memory=%ld\n", __FUNCTION__, rusage->total_memory);
+	dkprintf("%s: rusage.total_memory=%ld\n", __FUNCTION__, rusage.total_memory);
 }
 
 #ifdef POSTK_DEBUG_TEMP_FIX_73 /* NULL access for *monitor fix */
@@ -1769,7 +1766,7 @@ void mem_init(void)
 	monitor_init();
 #endif /* !POSTK_DEBUG_TEMP_FIX_73 */
 
-	/* It must precedes numa_init() because rusage->total_memory is initialized in numa_init() */
+	/* It must precedes numa_init() because rusage.total_memory is initialized in numa_init() */
 	rusage_init();
 
 	/* Initialize NUMA information and memory allocator bitmaps */
