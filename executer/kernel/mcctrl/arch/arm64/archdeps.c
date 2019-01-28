6cf8747113aa050ab313cba14380dae474a74adf
Balazs Gerofi <bgerofi@riken.jp>
2019-01-28 11:06:30 +0900

    Fix ThunderX2 write-combined PTE flag insanity
    Change-Id: I59999a680b556acf3e22ac516f4758e3aee7f355

--- executer/kernel/mcctrl/arch/arm64/archdeps.c
+++ executer/kernel/mcctrl/arch/arm64/archdeps.c
@@ -326,14 +326,6 @@ out:
 	return error;
 }
 
-#ifdef POSTK_DEBUG_ARCH_DEP_12
-#define PFN_WRITE_COMBINED PTE_ATTRINDX(MT_NORMAL_NC)
-static inline bool pte_is_write_combined(pte_t pte)
-{
-	return ((pte_val(pte) & PTE_ATTRINDX_MASK) == PFN_WRITE_COMBINED);
-}
-#endif /* POSTK_DEBUG_ARCH_DEP_12 */
-
 long mcexec_uti_save_fs(ihk_os_t os, struct uti_save_fs_desc __user *udesc,
 			struct file *file)
 {
