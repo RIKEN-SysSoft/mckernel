d2442935e6a2f90058ec1cf1a50904cdd4c1c908
Balazs Gerofi <bgerofi@riken.jp>
2019-01-28 11:06:30 +0900

    Fix ThunderX2 write-combined PTE flag insanity
    Change-Id: I59999a680b556acf3e22ac516f4758e3aee7f355

--- arch/arm64/kernel/memory.c
+++ arch/arm64/kernel/memory.c
@@ -15,6 +15,15 @@
 #include <kmalloc.h>
 #include <vdso.h>
 #include <debug.h>
+#include <rusage_private.h>
+#include <cputype.h>
+
+//#define DEBUG
+
+#ifdef DEBUG
+#undef DDEBUG_DEFAULT
+#define DDEBUG_DEFAULT DDEBUG_PRINT
+#endif
 
 #define NOT_IMPLEMENTED()  do { kprintf("%s is not implemented\n", __func__); while(1);} while(0)
 
@@ -1000,7 +1009,19 @@ static unsigned long attr_to_pageattr(enum ihk_mc_pt_attribute attr)
  	if (attr & PTATTR_UNCACHABLE) {
 		pte |= PROT_DEFAULT | PTE_ATTRINDX(MT_DEVICE_nGnRE);
 	} else if (attr & PTATTR_WRITE_COMBINED) {
-		pte |= PROT_DEFAULT | PTE_ATTRINDX(MT_NORMAL_NC);
+		switch (read_cpuid_id() & MIDR_CPU_MODEL_MASK) {
+			/*
+			 * Fix up arm64 braindamage of using NORMAL_NC for write
+			 * combining when Device GRE exists specifically for the
+			 * purpose. Needed on ThunderX2.
+			 */
+			case MIDR_CPU_MODEL(ARM_CPU_IMP_BRCM, BRCM_CPU_PART_VULCAN):
+			case MIDR_CPU_MODEL(ARM_CPU_IMP_CAVIUM, CAVIUM_CPU_PART_THUNDERX2):
+				pte |= PROT_DEFAULT | PTE_ATTRINDX(MT_DEVICE_GRE);
+				break;
+			default:
+				pte |= PROT_DEFAULT | PTE_ATTRINDX(MT_NORMAL_NC);
+		}
 	} else {
 		pte |= PROT_DEFAULT | PTE_ATTRINDX(MT_NORMAL);
 	}
--- executer/kernel/mcctrl/arch/arm64/include/archdeps.h
+++ executer/kernel/mcctrl/arch/arm64/include/archdeps.h
@@ -7,10 +7,25 @@ extern int translate_rva_to_rpa(ihk_os_t os, unsigned long rpt, unsigned long rv
 				unsigned long *rpap, unsigned long *pgsizep);
 
 #ifdef POSTK_DEBUG_ARCH_DEP_12
-#define PFN_WRITE_COMBINED PTE_ATTRINDX(MT_NORMAL_NC)
 
+#define PFN_WRITE_COMBINED PTE_ATTRINDX(MT_NORMAL_NC)
 static inline bool pte_is_write_combined(pte_t pte)
 {
+#if defined(MIDR_CPU_MODEL_MASK)
+	/*
+	 * Fix up arm64 braindamage of using NORMAL_NC for write
+	 * combining when Device GRE exists specifically for the
+	 * purpose. Needed on ThunderX2.
+	 */
+	switch (read_cpuid_id() & MIDR_CPU_MODEL_MASK) {
+#if defined(ARM_CPU_IMP_BRCM) && defined(BRCM_CPU_PART_VULCAN)
+		case MIDR_CPU_MODEL(ARM_CPU_IMP_BRCM, BRCM_CPU_PART_VULCAN):
+#endif
+		case MIDR_CPU_MODEL(ARM_CPU_IMP_CAVIUM, CAVIUM_CPU_PART_THUNDERX2):
+			return ((pte_val(pte) & PTE_ATTRINDX_MASK) ==
+					PTE_ATTRINDX(MT_DEVICE_GRE));
+	}
+#endif
 	return ((pte_val(pte) & PTE_ATTRINDX_MASK) == PFN_WRITE_COMBINED);
 }
 #endif /* POSTK_DEBUG_ARCH_DEP_12 */
