0a2dc60ba45e88c5e0085b568f1f6cf52fdd58f9
Masamichi Takagi <masamichi.takagi@riken.jp>
2019-01-29 09:42:49 +0900

    coredump: kzalloc ELF header and memory range table
    Change-Id: I0ff38c1b0e1e6ef204cb3605c0178848dbe40bfb
    Fujitsu: POSTK_TEMP_FIX_63

--- arch/x86_64/kernel/gencore.c
+++ arch/x86_64/kernel/gencore.c
@@ -356,14 +356,11 @@ int gencore(struct thread *thread, void *regs,
 
 	dkprintf("now generate a core file image\n");
 
-	eh = kmalloc(sizeof(*eh), IHK_MC_AP_NOWAIT);
+	eh = kzalloc(sizeof(*eh), IHK_MC_AP_NOWAIT);
 	if (eh == NULL) {
 		dkprintf("could not alloc a elf header table.\n");
 		goto fail;
 	}
-#ifdef POSTK_DEBUG_TEMP_FIX_63 /* Add core table and elf header initialization */
-	memset(eh, 0, sizeof(*eh));
-#endif /* POSTK_DEBUG_TEMP_FIX_63 */
 
 	offset += sizeof(*eh);
 	fill_elf_header(eh, segs);
@@ -433,7 +430,7 @@ int gencore(struct thread *thread, void *regs,
 	}
 
 	/* coretable to send to host */
-	ct = kmalloc(sizeof(struct coretable) * (*chunks), IHK_MC_AP_NOWAIT);
+	ct = kzalloc(sizeof(struct coretable) * (*chunks), IHK_MC_AP_NOWAIT);
 	if (!ct) {
 		dkprintf("could not alloc a coretable.\n");
 		goto fail;
