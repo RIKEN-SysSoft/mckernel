07aa96ef95d1a5a7e7cb97bd140c8472ac0429d3
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2019-02-21 11:29:33 +0900

    arm64: Scalable Vector Extension (SVE) support.
    Change-Id: I3568687913f583edfaa297d5cf5ac91d319d97e9

--- arch/arm64/kernel/include/prctl.h
+++ arch/arm64/kernel/include/prctl.h
@@ -1,4 +1,4 @@
-/* prctl.h COPYRIGHT FUJITSU LIMITED 2017 */
+/* prctl.h COPYRIGHT FUJITSU LIMITED 2017-2019 */
 #ifndef __HEADER_ARM64_COMMON_PRCTL_H
 #define __HEADER_ARM64_COMMON_PRCTL_H
 
@@ -6,15 +6,12 @@
 #define PR_GET_THP_DISABLE 42
 
 /* arm64 Scalable Vector Extension controls */
-#define PR_SVE_SET_VL		48			/* set task vector length */
-#define PR_SVE_SET_VL_THREAD	(1 << 1)		/* set just this thread */
-#define PR_SVE_SET_VL_INHERIT	(1 << 2)		/* inherit across exec */
-#define PR_SVE_SET_VL_ONEXEC	(1 << 3)		/* defer effect until exec */
-
-#define PR_SVE_GET_VL		49			/* get task vector length */
-/* Decode helpers for the return value from PR_SVE_GET_VL: */
-#define PR_SVE_GET_VL_LEN(ret)	((ret) & 0x3fff)	/* vector length */
-#define PR_SVE_GET_VL_INHERIT	(PR_SVE_SET_VL_INHERIT << 16)
-/* For conveinence, PR_SVE_SET_VL returns the result in the same encoding */
+/* Flag values must be kept in sync with ptrace NT_ARM_SVE interface */
+#define PR_SVE_SET_VL		50		/* set task vector length */
+# define PR_SVE_SET_VL_ONEXEC	(1 << 18)	/* defer effect until exec */
+#define PR_SVE_GET_VL		51		/* get task vector length */
+/* Bits common to PR_SVE_SET_VL and PR_SVE_GET_VL */
+# define PR_SVE_VL_LEN_MASK	0xffff
+# define PR_SVE_VL_INHERIT	(1 << 17)	/* inherit across exec */
 
 #endif /* !__HEADER_ARM64_COMMON_PRCTL_H */
