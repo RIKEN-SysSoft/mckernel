8f822ebf219eb93fda5cf4792dd96a0c7b6dac1a
Masamichi Takagi <masamichi.takagi@riken.jp>
2019-02-24 17:24:22 +0900

    copy_user_pte: vmap area not owned by McKernel
    Change-Id: Iae0f1145d58ec2c14cecc14409b08a1db3b067b7

--- test/issues/1166/CT1166.sh
+++ test/issues/1166/CT1166.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
-USELTP=0
+USELTP=1
 USEOSTEST=0
 
-BOOTPARAM="-c 1-3 -m 1G"
+BOOTPARAM="-s -c 1-3 -m 1G"
 . ../../common.sh
 
 ################################################################################
-$MCEXEC ./CT1166
+sudo $MCEXEC ./CT1166
+
+for i in fork01:09 fork02:10 fork03:11 fork04:12 fork07:13 fork08:14 fork09:15 \
+	 fork10:16 fork11:17; do
+	tp=`echo $i|sed 's/:.*//'`
+	id=`echo $i|sed 's/.*://'`
+	sudo $MCEXEC $LTPBIN/$tp 2>&1 | tee $tp.txt
+	ok=`grep TPASS $tp.txt | wc -l`
+	ng=`grep TFAIL $tp.txt | wc -l`
+	if [ $ng = 0 ]; then
+		echo "*** CT1166$id: $tp OK ($ok)"
+	else
+		echo "*** CT1166$id: $tp NG (ok=$ok ng=%ng)"
+	fi
+done
