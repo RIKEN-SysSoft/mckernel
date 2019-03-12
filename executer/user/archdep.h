6e89d359263313693878bff237245235e9a1afea
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2019-03-12 14:35:27 +0900

    uti 'fs' functions: rename x86-specific 'fs' to 'tls' + arm implem
    Note: the original fujitsu implementation didn't rename the various
    save_fs function/desc to save_tls for some reason, might as well go all
    the way though...
    
    Change-Id: Ic362c15c8b320c4d258d2ead8c5fd4eafd9d0ae9
    Fujitsu: POSTK_DEBUG_ARCH_DEP_91

--- executer/user/archdep.h
+++ executer/user/archdep.h
@@ -1,6 +1,6 @@
 #include "../include/uprotocol.h"
 
-extern int switch_ctx(int fd, unsigned long cmd, struct uti_save_fs_desc *desc, void *lctx, void *rctx);
+extern int switch_ctx(int fd, unsigned long cmd, struct uti_save_tls_desc *desc, void *lctx, void *rctx);
 extern unsigned long compare_and_swap(unsigned long *addr, unsigned long old, unsigned long new);
 extern unsigned int compare_and_swap_int(unsigned int *addr, unsigned int old, unsigned int new);
 extern int archdep_syscall(struct syscall_wait_desc *w, long *ret);
