2cee82673b52d3c07e15564f12c3c24f7fce73c7
Masamichi Takagi <masamichi.takagi@riken.jp>
2019-03-29 05:34:44 +0000

    test: perf_event_open: Fix test program
    Change-Id: Ie5af8fb3ab7452078f2c35ec14c6369d86eedec3

--- test/perf_event/go_perf_test.sh
+++ test/perf_event/go_perf_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MCEXEC="mcexec"
+. ../common.sh
 
 PERF_HW_ID_MAX=9
 PERF_COUNT_HW_CACHE_MAX=6
@@ -10,56 +10,56 @@ PERF_COUNT_HW_CACHE_RESULT_MAX=1
 PERF_TYPE_HARDWARE=0
 PERF_TYPE_HW_CACHE=3
 
-echo "【PERF_TYPE_HARDWARE all spase】, "
+echo "[PERF_TYPE_HARDWARE all space]"
 for id in `seq 0 ${PERF_HW_ID_MAX}`
 do
-	${MCEXEC} ./perf_test 0 1 ${PERF_TYPE_HARDWARE} ${id}
+	${MCK_DIR}/bin/mcexec ./perf_test 0 1 ${PERF_TYPE_HARDWARE} ${id}
 done
 
-echo "【HW_CACHE no exclude】, "
+echo "[HW_CACHE no exclude]"
 for i in `seq 0 ${PERF_COUNT_HW_CACHE_MAX}`
 do
 	for j in `seq 0 ${PERF_COUNT_HW_CACHE_OP_MAX}`
 	do
 		for k in `seq 0 ${PERF_COUNT_HW_CACHE_RESULT_MAX}`
 		do
-			${MCEXEC} ./perf_test 0 1 ${PERF_TYPE_HW_CACHE} `expr ${k} \* 65536 + ${j} \* 256 + ${i}`
+			${MCK_DIR}/bin/mcexec ./perf_test 0 1 ${PERF_TYPE_HW_CACHE} `expr ${k} \* 65536 + ${j} \* 256 + ${i}`
 		done
 	done
 done
 
-echo "【HARDWARE exclude user space】, "
+echo "[HARDWARE exclude user space]"
 for id in `seq 0 ${PERF_HW_ID_MAX}`
 do
-	${MCEXEC} ./perf_test 1 1 ${PERF_TYPE_HARDWARE} ${id}
+	${MCK_DIR}/bin/mcexec ./perf_test 1 1 ${PERF_TYPE_HARDWARE} ${id}
 done
 
-echo "【HW_CACHE exclude user space】, "
+echo "[HW_CACHE exclude user space]"
 for i in `seq 0 ${PERF_COUNT_HW_CACHE_MAX}`
 do
 	for j in `seq 0 ${PERF_COUNT_HW_CACHE_OP_MAX}`
 	do
 		for k in `seq 0 ${PERF_COUNT_HW_CACHE_RESULT_MAX}`
 		do
-			${MCEXEC} ./perf_test 1 1 ${PERF_TYPE_HW_CACHE} `expr ${k} \* 65536 + ${j} \* 256 + ${i}`
+			${MCK_DIR}/bin/mcexec ./perf_test 1 1 ${PERF_TYPE_HW_CACHE} `expr ${k} \* 65536 + ${j} \* 256 + ${i}`
 		done
 	done
 done
 
-echo "【HARDWARE exclude kernel space】, "
+echo "[HARDWARE exclude kernel space]"
 for id in `seq 0 ${PERF_HW_ID_MAX}`
 do
-	${MCEXEC} ./perf_test 2 1 ${PERF_TYPE_HARDWARE} ${id}
+	${MCK_DIR}/bin/mcexec ./perf_test 2 1 ${PERF_TYPE_HARDWARE} ${id}
 done
 
-echo "【HW_CACHE exclude kernel space】, "
+echo "[HW_CACHE exclude kernel space]"
 for i in `seq 0 ${PERF_COUNT_HW_CACHE_MAX}`
 do
 	for j in `seq 0 ${PERF_COUNT_HW_CACHE_OP_MAX}`
 	do
 		for k in `seq 0 ${PERF_COUNT_HW_CACHE_RESULT_MAX}`
 		do
-			${MCEXEC} ./perf_test 2 1 ${PERF_TYPE_HW_CACHE} `expr ${k} \* 65536 + ${j} \* 256 + ${i}`
+			${MCK_DIR}/bin/mcexec ./perf_test 2 1 ${PERF_TYPE_HW_CACHE} `expr ${k} \* 65536 + ${j} \* 256 + ${i}`
 		done
 	done
 done
