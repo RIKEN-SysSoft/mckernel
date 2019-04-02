7fee6cc72118c10242764529de8483d634cba25b
Masamichi Takagi <masamichi.takagi@riken.jp>
2019-04-02 04:33:13 +0000

    (Skelton, please update) perf_event_open: Support PERF_COUNT_HW_REF_CPU_CYCLES
    Change-Id: If2ac2dfa182f3c16ff48210ad81f120d7e72514b

---
+++ test/issues/1025/C1025_linux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+MCEXEC=
+
+PERF_TYPE_HARDWARE=0
+PERF_COUNT_HW_REF_CPU_CYCLES=9
+
+echo "[type: HARDWARE, counter: HW_REF_CPU_CYCLES, exclude: none]"
+${MCEXEC} ./perf_test 0 1 ${PERF_TYPE_HARDWARE} ${PERF_COUNT_HW_REF_CPU_CYCLES}
+
+echo "[type: HARDWARE, counter: HW_REF_CPU_CYCLES, exclude: user]"
+${MCEXEC} ./perf_test 1 1 ${PERF_TYPE_HARDWARE} ${PERF_COUNT_HW_REF_CPU_CYCLES}
+
+echo "[type: HARDWARE, counter: HW_REF_CPU_CYCLES, exclude: kernel]"
+echo "[HARDWARE exclude kernel space]"
+${MCEXEC} ./perf_test 2 1 ${PERF_TYPE_HARDWARE} ${PERF_COUNT_HW_REF_CPU_CYCLES}
