3b74b0a093474c09c6235e9373d79717a2950c0d
Masamichi Takagi <masamichi.takagi@riken.jp>
2019-06-21 19:13:28 +0900

    rusage: Move pgsize_to_pgshift to arch-memory.h
    Change-Id: Ia10b6e5c7d078d345347a79a3e98c06c16d28d6a

--- kernel/include/rusage.h
+++ kernel/include/rusage.h
@@ -9,45 +9,6 @@
 
 extern struct rusage_global rusage;
 
-static inline int rusage_pgsize_to_pgtype(size_t pgsize)
-{
-	int ret = IHK_OS_PGSIZE_4KB;
-	int pgshift = pgsize_to_pgshift(pgsize);
-
-	switch (pgshift) {
-	case 12:
-		ret = IHK_OS_PGSIZE_4KB;
-		break;
-	case 16:
-		ret = IHK_OS_PGSIZE_64KB;
-		break;
-	case 21:
-		ret = IHK_OS_PGSIZE_2MB;
-		break;
-	case 25:
-		ret = IHK_OS_PGSIZE_32MB;
-		break;
-	case 30:
-		ret = IHK_OS_PGSIZE_1GB;
-		break;
-	case 34:
-		ret = IHK_OS_PGSIZE_16GB;
-		break;
-	case 29:
-		ret = IHK_OS_PGSIZE_512MB;
-		break;
-	case 42:
-		ret = IHK_OS_PGSIZE_4TB;
-		break;
-	default:
-		kprintf("%s: Error: Unknown pgsize=%ld\n",
-			__func__, pgsize);
-		break;
-	}
-
-	return ret;
-}
-
 struct rusage_percpu {
 	unsigned long user_tsc;
 	unsigned long system_tsc;
