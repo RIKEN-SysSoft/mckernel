d017ea6ce4141f372355f36e3b9e572d59e1fc3a
Tomoki Shirasawa <tomoki.shirasawa.kk@hitachi-solutions.com>
2019-07-25 13:52:23 +0900

    syscall offload regardless of mcexec life and death
    Change-Id: I7db089993d3ee5ae6032f5085db2b67cef99fdfb
    Refs: #1321

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -181,7 +181,6 @@ long do_syscall(struct syscall_request *req, int cpu)
 	struct syscall_response res;
 	long rc;
 	struct thread *thread = cpu_local_var(current);
-	struct process *proc = thread->proc;
 	struct ihk_os_cpu_monitor *monitor = cpu_local_var(monitor);
 	int mstatus = 0;
 
@@ -204,9 +203,6 @@ long do_syscall(struct syscall_request *req, int cpu)
 	barrier();
 
 	if(req->number != __NR_exit_group){
-		if (proc->nohost) {// host is down
-			return -EPIPE;
-		}
 		++thread->in_syscall_offload;
 	}
 
