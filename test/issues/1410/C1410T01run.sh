f844945f97ae4a940e7b0cf222c6b349eb2a588d
Tomoki Shirasawa <tomoki.shirasawa.kk@hitachi-solutions.com>
2019-11-14 11:35:01 +0900

    SIGCONT: don't terminate process
    Change-Id: Ib959a9e5341fda37bd055724ecb9319a469b7420
    Refs: #1410

---
+++ test/issues/1410/C1410T01run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+sh ./C1410T01.sh 2>&1 | tee C1410T01.log
+awk '
+/send (SIGCONT|SIGKILL)/{
+	if (st>0) {
+		if (n > 5) {
+			ok++
+		}
+		else{
+			ng++
+		}
+	}
+	st++
+	n=0
+	next
+}
+{n++}
+END{
+	if (ng > 0) {
+		printf("*** C1410T01 FAIL ng=%d ok=%d\n", ng, ok)
+		exit(1)
+	}
+	else {
+		printf("*** C1410T01 PASS ng=%d ok=%d\n", ng, ok)
+		exit(0)
+	}
+}' C1410T01.log
