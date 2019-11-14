3e568f7e5a2368a749452b25864471e7ffa5ba0a
Balazs Gerofi <bgerofi@riken.jp>
2019-11-14 09:15:56 +0900

    Record pthread routine address in clone(), keep helper threads on caller CPU core
    Change-Id: I29d1589e430dc1396558cdf3df4d068c27173612

--- kernel/include/process.h
+++ kernel/include/process.h
@@ -609,6 +609,7 @@ struct thread {
 	// thread info
 	int cpu_id;
 	int tid;
+	char pthread_routine[PATH_MAX + 64];
 	int status;	// PS_RUNNING -> PS_EXITED (-> ZOMBIE / ptrace)
 			// |       ^       ^
 			// |       |       |
@@ -718,6 +719,7 @@ struct thread {
 	/* Syscall offload wait queue head */
 	struct waitq scd_wq;
 
+	unsigned long clone_pthread_start_routine;
 	int uti_state;
 	int mod_clone;
 	struct uti_attr *mod_clone_arg;
