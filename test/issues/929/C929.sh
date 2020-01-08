519e8fbc6378443080705d108a039123c66001b0
Ken Sato <ken.sato.ty@hitachi-solutions.com>
2020-01-08 12:06:43 +0900

    mcexec: make to return error when execed by exceed -n
    Change-Id: Iaf5cfb11c37bea6957b77a2114f783e9a46a48f2
    Refs: #929

---
+++ test/issues/929/C929.sh
@@ -0,0 +1,100 @@
+#/bin/sh
+
+USELTP=0
+USEOSTEST=0
+
+. ../../common.sh
+
+issue="929"
+tid=01
+
+tname=`printf "C${issue}T%02d" ${tid}`
+echo "*** ${tname} start *******************************"
+TEST_CMD="mpirun -f ./hostfile -ppn 5 ${MCEXEC} -n 5 ./test_prog.sh"
+echo ${TEST_CMD}
+${TEST_CMD} &> ${tname}.txt
+mpi_ret=$?
+
+cat ./${tname}.txt
+started_num=`grep 'test_prog is started' ./${tname}.txt | wc -l`
+
+if [ ${mpi_ret} -eq 0 -a ${started_num} -eq 5 ]; then
+	echo "*** ${tname} PASSED ******************************"
+else
+	echo "*** ${tname} FAILED ******************************"
+fi
+let tid++
+echo ""
+
+tname=`printf "C${issue}T%02d" ${tid}`
+echo "*** ${tname} start *******************************"
+TEST_CMD="mpirun -f ./hostfile -ppn 5 ${MCEXEC} -n 3 ./test_prog.sh"
+echo ${TEST_CMD}
+${TEST_CMD} &> ${tname}.txt
+mpi_ret=$?
+
+cat ./${tname}.txt
+started_num=`grep 'test_prog is started' ./${tname}.txt | wc -l`
+
+if [ ${mpi_ret} -ne 0 -a ${started_num} -eq 3 ]; then
+	echo "*** ${tname} PASSED ******************************"
+else
+	echo "*** ${tname} FAILED ******************************"
+fi
+let tid++
+echo ""
+
+tname=`printf "C${issue}T%02d" ${tid}`
+echo "*** ${tname} start *******************************"
+TEST_CMD="mpirun -f ./hostfile -ppn 3 ${MCEXEC} -n 5 ./test_prog.sh"
+echo ${TEST_CMD}
+${TEST_CMD} &> ${tname}.txt
+mpi_ret=$?
+
+cat ./${tname}.txt
+started_num=`grep 'test_prog is started' ./${tname}.txt | wc -l`
+
+if [ ${mpi_ret} -ne 0 -a ${started_num} -eq 0 ]; then
+	echo "*** ${tname} PASSED ******************************"
+else
+	echo "*** ${tname} FAILED ******************************"
+fi
+let tid++
+echo ""
+
+tname=`printf "C${issue}T%02d" ${tid}`
+echo "*** ${tname} start *******************************"
+TEST_CMD="mpirun -f ./hostfile -ppn 6 ${MCEXEC} -n 3 ./test_prog.sh"
+echo ${TEST_CMD}
+${TEST_CMD} &> ${tname}.txt
+mpi_ret=$?
+
+cat ./${tname}.txt
+started_num=`grep 'test_prog is started' ./${tname}.txt | wc -l`
+
+if [ ${mpi_ret} -ne 0 -a ${started_num} -eq 3 ]; then
+	echo "*** ${tname} PASSED ******************************"
+else
+	echo "*** ${tname} FAILED ******************************"
+fi
+let tid++
+echo ""
+
+tname=`printf "C${issue}T%02d" ${tid}`
+echo "*** ${tname} start *******************************"
+TEST_CMD="mpirun -f ./hostfile -ppn 250 ${MCEXEC} -n 250 ./test_prog.sh"
+echo ${TEST_CMD}
+${TEST_CMD} &> ${tname}.txt
+mpi_ret=$?
+
+head -n 10 ./${tname}.txt
+echo "..."
+started_num=`grep 'test_prog is started' ./${tname}.txt | wc -l`
+
+if [ ${mpi_ret} -ne 0 -a ${started_num} -eq 0 ]; then
+	echo "*** ${tname} PASSED ******************************"
+else
+	echo "*** ${tname} FAILED ******************************"
+fi
+let tid++
+echo ""
