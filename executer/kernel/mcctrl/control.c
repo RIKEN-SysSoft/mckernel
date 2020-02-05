311b89142fc047a7c6dc484f9b97ce1693698bba
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-02-05 15:12:56 +0900

    __mcctrl_control: Check privilege
    Change-Id: I2f5019135039cde1329b80de07227198969e9978

--- executer/kernel/mcctrl/control.c
+++ executer/kernel/mcctrl/control.c
@@ -3186,9 +3186,45 @@ out:
 	return rc;
 }
 
+static int __mcctrl_control_perm(unsigned int request)
+{
+	int ret = 0;
+	kuid_t euid;
+
+	/* blacklist */
+	switch (request) {
+	case IHK_OS_AUX_PERF_NUM:
+	case IHK_OS_AUX_PERF_SET:
+	case IHK_OS_AUX_PERF_GET:
+	case IHK_OS_AUX_PERF_ENABLE:
+	case IHK_OS_AUX_PERF_DISABLE:
+	case IHK_OS_AUX_PERF_DESTROY:
+		euid = current_euid();
+		dprintf("%s: request: %x, euid: %u\n",
+			__func__, request, euid.val);
+		if (euid.val) {
+			ret = -EPERM;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
+
 long __mcctrl_control(ihk_os_t os, unsigned int req, unsigned long arg,
                       struct file *file)
 {
+	int ret;
+
+	ret = __mcctrl_control_perm(req);
+	if (ret) {
+		dprintf("%s: permission denied, req: %x, ret: %d\n",
+			__func__, req, ret);
+		return ret;
+	}
+
 	switch (req) {
 	case MCEXEC_UP_PREPARE_IMAGE:
 		return mcexec_prepare_image(os,
