10e6362240b3bc50af574120de83bb55f36e2d53
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-03-17 14:44:25 +0900

    Revert "Record pthread routine address in clone(), keep helper threads on caller CPU core"
    This reverts commit fabe6a4c7351c02148da4d799e1e830a4fd05347.
    
    Change-Id: I4a4624f4bfb9d272d5ffd04f8fd730e68f4ba280

--- kernel/include/process.h
+++ kernel/include/process.h
@@ -608,7 +608,6 @@ struct thread {
 	// thread info
 	int cpu_id;
 	int tid;
-	char pthread_routine[PATH_MAX + 64];
 	int status;	// PS_RUNNING -> PS_EXITED (-> ZOMBIE / ptrace)
 			// |       ^       ^
 			// |       |       |
@@ -718,7 +717,6 @@ struct thread {
 	/* Syscall offload wait queue head */
 	struct waitq scd_wq;
 
-	unsigned long clone_pthread_start_routine;
 	int uti_state;
 	int mod_clone;
 	struct uti_attr *mod_clone_arg;
