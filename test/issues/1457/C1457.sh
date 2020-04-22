fd941dad44344298c5df5588f59fa2bc4f006eee
Balazs Gerofi <bgerofi@riken.jp>
2020-04-22 09:33:49 +0900

    Revert "procfs cpuinfo: use sequence number as processor"
    This reverts commit bb7e14065578b14498f00d65437fedf2802da172.
    
    Change-Id: If0c1719986706511c1e57d06bc61923d1adfc0aa

--- test/issues/1457/C1457.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. ${HOME}/.mck_test_config
-export MCK_DIR
-sudo ${MCK_DIR}/sbin/mcreboot.sh 1,3,5,7,9-12,50-58
-${MCK_DIR}/bin/mcexec python C1457.py
-sudo ${MCK_DIR}/sbin/mcstop+release.sh
