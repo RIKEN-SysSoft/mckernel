8a31fa63bbe145a3bdfddd8f17f0741791060147
Balazs Gerofi <bgerofi@riken.jp>
2020-05-27 16:15:55 +0900

    memclear: non-temporal memory clean (arm64)
    Change-Id: I8f80ff20e98bc01088450282e1790c27c67c16eb

--- arch/arm64/kernel/include/arch-string.h
+++ arch/arm64/kernel/include/arch-string.h
@@ -10,4 +10,13 @@ extern void *__inline_memcpy(void *to, const void *from, size_t t);
 
 extern void *__inline_memset(void *s, unsigned long c, size_t count);
 
+#define ARCH_MEMCLEAR
+
+extern void __memclear(void *addr, unsigned long len, void *tmp);
+inline static void memclear(void *addr, unsigned long len)
+{
+	uint64_t q0q1[4];
+	__memclear(addr, len, (void *)&q0q1);
+}
+
 #endif	/* __HEADER_ARM64_COMMON_ARCH_TIMER_H */
