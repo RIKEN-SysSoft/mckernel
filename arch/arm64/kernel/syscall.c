29b8ae23667933f5c6cfbc64e0fb375895a40671
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-06-12 11:52:27 +0900

    TO_REVERT do_process_vm_read_writev: don't check memory_range
    Change-Id: I4e099622a1121843c295706d2ad889604c83e5ab

--- arch/arm64/kernel/syscall.c
+++ arch/arm64/kernel/syscall.c
@@ -2181,6 +2181,7 @@ int do_process_vm_read_writev(int pid,
 		return -EINVAL;
 	}
 
+#if 0
 	/* Check if parameters are okay */
 	ihk_mc_spinlock_lock_noirq(&lthread->vm->memory_range_lock);
 
@@ -2209,6 +2210,7 @@ arg_out:
 	if (ret != 0) {
 		goto out;
 	}
+#endif
 
 	for (li = 0; li < liovcnt; ++li) {
 		llen += local_iov[li].iov_len;
