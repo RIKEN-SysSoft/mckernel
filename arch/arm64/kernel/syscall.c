901a348dfa45652c3e0a8e716e141d4925241dec
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-06-12 11:52:27 +0900

    do_process_vm_read_writev: don't check vm_range (workaround for Fugaku)
    Change-Id: I4ce9b5397ed876dff651c67658e43811d83658dd

--- arch/arm64/kernel/syscall.c
+++ arch/arm64/kernel/syscall.c
@@ -2141,7 +2141,9 @@ int do_process_vm_read_writev(int pid,
 	unsigned long lpage_left, rpage_left;
 	unsigned long lpsize, rpsize;
 	void *rva, *lva;
+#if 0
 	struct vm_range *range;
+#endif
 	struct mcs_rwlock_node_irqsave lock;
 	struct mcs_rwlock_node update_lock;
 
@@ -2154,6 +2156,7 @@ int do_process_vm_read_writev(int pid,
 		return -EINVAL;
 	}
 
+#if 0
 	/* Check if parameters are okay */
 	ihk_rwspinlock_read_lock_noirq(&lthread->vm->memory_range_lock);
 
@@ -2182,6 +2185,7 @@ arg_out:
 	if (ret != 0) {
 		goto out;
 	}
+#endif
 
 	for (li = 0; li < liovcnt; ++li) {
 		llen += local_iov[li].iov_len;
