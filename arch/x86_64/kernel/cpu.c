2d4aea8b9bae13ebc4af2d27294ac0e7036e37a9
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-06-15 12:51:48 +0900

    TO RESET: unhandled_page_fault: show instruction address
    Change-Id: I29a8d30d9b3e5cfbe5e16b1faaa253e794b8fc5b

--- arch/x86_64/kernel/cpu.c
+++ arch/x86_64/kernel/cpu.c
@@ -1530,6 +1530,14 @@ void arch_print_stack(void)
 	__print_stack(rbp, 0);
 }
 
+unsigned long arch_get_instruction_address(const void *reg)
+{
+	const struct x86_user_context *uctx = reg;
+	const struct x86_basic_regs *regs = &uctx->gpr;
+
+	return regs->rip;
+}
+
 /*@
   @ requires \valid(reg);
   @ assigns \nothing;
