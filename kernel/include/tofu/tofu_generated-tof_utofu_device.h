f68a29b6393a142405d7d24c3d85adeb545ebc66
Balazs Gerofi <bgerofi@riken.jp>
2020-09-06 12:07:51 +0900

    Tofu: support for barrier gate, kmalloc cache
    Change-Id: I6f4cfec2ec404efd03b332fc3f449a775816230e

--- kernel/include/tofu/tofu_generated-tof_utofu_device.h
+++ kernel/include/tofu/tofu_generated-tof_utofu_device.h
@@ -5,5 +5,13 @@ struct tof_utofu_device {
 			char padding0[0];
 			bool enabled;
 		};
+		struct {
+			char padding1[12];
+			uint32_t gpid;
+		};
+		struct {
+			char padding2[24];
+			uint64_t subnet;
+		};
 	};
 };
