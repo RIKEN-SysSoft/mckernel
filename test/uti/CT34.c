d01b2b8ae888d5cbf4e8e6dfcbdf4488a1a611c5
Ken Sato <ken.sato.ty@hitachi-solutions.com>
2020-09-18 14:42:26 +0900

    uti: futex call function in mcctrl
    Previously, futex code of McKerenl was called by mccontrol,
    but there ware some problems with this method.
    (Mainly, location of McKernel image on memory)
    
    Call futex code in mcctrl instead of the one in McKernel image,
    giving the following benefits:
    1. Not relying on shared kernel virtual address space with Linux any more
    2. The cpu id store / retrieve is not needed and resulting in the code
    
    Change-Id: Ic40929b64a655b270c435859fa287fedb713ee5c
    refe: #1428

--- test/uti/CT34.c
+++ test/uti/CT34.c
@@ -1,62 +1,158 @@
 #define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <pthread.h>
 #include <errno.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdint.h>
-#include <sys/syscall.h>
 #include <sys/mman.h>
-#include <sched.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/futex.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <uti.h>
 #include "util.h"
+
+#define WAITER_CPU 0
+#define WAKER_CPU 1
+
+int sem;
+pthread_barrier_t bar;
+int flag;
+pthread_t thr;
+long t_futex_wait, t_fwq;
+long nloop;
+long blocktime = 10L * 1000 * 1000;
 
 void *util_fn(void *arg)
 {
+	int i;
 	int ret;
+    long start, end;
+	int testid = 32101;
+
+	print_cpu_last_executed_on("Utility thread");
+
 	ret = syscall(732);
 	OKNGNOJUMP(ret == -1, "Utility thread is running on Linux\n");
+
+	pthread_barrier_wait(&bar);
+	start = rdtsc_light();
+	for (i = 0; i < nloop; i++) {
+		
+		if ((ret = syscall(__NR_futex, &sem, FUTEX_WAIT, 0, NULL, NULL, 0))) {
+			printf("Error: futex wait failed (%s)\n", strerror(errno));
+		}
+
+		//pthread_barrier_wait(&bar); /* 2nd futex */
+	}
+	end = rdtsc_light();
+	t_futex_wait += end - start;
+
+	ret = 0;
  fn_fail:
 	return NULL;
 }
 
-int my_thread_create()
+static struct option options[] = {
+	/* end */
+	{ NULL, 0, NULL, 0, }
+};
+
+int main(int argc, char **argv)
 {
-	pthread_t thr;
-	int ret = 0;
+	int i;
+	int ret;
+    long start, end;
+	cpu_set_t cpuset;
+	pthread_attr_t attr;
+	pthread_barrierattr_t bar_attr;
+	struct sched_param param = { .sched_priority = 99 };
+	int opt;
+
+	while ((opt = getopt_long(argc, argv, "+b:", options, NULL)) != -1) {
+		switch (opt) {
+			case 'b':
+				blocktime = atoi(optarg);
+				break;
+			default: /* '?' */
+				printf("unknown option %c\n", optopt);
+				exit(1);
+		}
+	}
+	nloop = (10 * 1000000000UL) / blocktime;
+	printf("[INFO] nloop=%ld,blocktime=%ld\n", nloop, blocktime);
+
+	
+ 	CPU_ZERO(&cpuset);
+	CPU_SET(WAKER_CPU, &cpuset);
+	if ((ret = sched_setaffinity(0, sizeof(cpu_set_t), &cpuset))) {
+ 		printf("Error: sched_setaffinity: %s\n", strerror(errno));
+		goto fn_fail;
+	}
+	print_cpu_last_executed_on("Master thread");
+
+	fwq_init();
+
+	pthread_barrierattr_init(&bar_attr);
+	pthread_barrier_init(&bar, &bar_attr, 2);
+
+	if ((ret = pthread_attr_init(&attr))) {
+ 		printf("Error: pthread_attr_init: %s\n", strerror(errno));
+		goto fn_fail;
+	}
+
+ 	CPU_ZERO(&cpuset);
+	CPU_SET(WAITER_CPU, &cpuset);
+
+	if ((ret = pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuset))) {
+ 		printf("Error: pthread_attr_setaffinity_np: %s\n", strerror(errno));
+		goto fn_fail;
+	}
+
+	ret = syscall(732);
+	OKNGNOJUMP(ret != -1, "Master thread is running on McKernel\n");
 
 	ret = syscall(731, 1, NULL);
-	OKNGNOJUMP(ret == 0, "util_indicate_clone,ret=%d,errno=%d\n", ret, errno);
+	OKNGNOJUMP(ret != -1, "util_indicate_clone\n");
 
-	if ((ret = pthread_create(&thr, NULL, util_fn, NULL))) {
+	if ((ret = pthread_create(&thr, &attr, util_fn, NULL))) {
 		printf("Error: pthread_create: %s\n", strerror(errno));
-	}
-	
-	if ((ret = pthread_join(thr, NULL))) {
-		printf("Error: pthread_join: %s\n", strerror(errno));
+		goto fn_fail;
 	}
 
- fn_exit:
-	return ret;
+	if ((ret = sched_setscheduler(0, SCHED_FIFO, &param))) {
+		printf("Error: sched_setscheduler: %s\n", strerror(errno));
+		ret = -errno;
+		goto fn_fail;
+	}
 
+	syscall(701, 1 | 2);
+	pthread_barrier_wait(&bar);
+	for (i = 0; i < nloop; i++) {
+		start = rdtsc_light();
+
+		fwq(blocktime);
+
+		end = rdtsc_light();
+		t_fwq += end - start;
+
+		if ((ret = syscall(__NR_futex, &sem, FUTEX_WAKE, 1, NULL, NULL, 0)) == -1) {
+			printf("Error: futex wake: %s\n", strerror(errno));
+		}
+
+		//pthread_barrier_wait(&bar);
+
+	}
+	syscall(701, 4 | 8);
+
+	pthread_join(thr, NULL);
+	printf("[INFO] waiter: %ld cycles, waker: %ld cycles, (waiter - waker) / nloop: %ld cycles\n", t_futex_wait, t_fwq, (t_futex_wait - t_fwq) / nloop);
+
+	ret = 0;
  fn_fail:
-	ret = -1;
-	goto fn_exit;
-}
-
-int
-main(int argc, char **argv)
-{
-	int ret = 0;
-
-	if ((ret = my_thread_create())) {
-		printf("Error: my_thread_create,ret=%d\n", ret);
-	}
-
- fn_exit:
 	return ret;
-
- fn_fail:
-	ret = -1;
-	goto fn_exit;
 }
