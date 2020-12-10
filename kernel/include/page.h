ee46c71108bffe491cab2f4fbc13aa1bc8d8fa38
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-12-10 15:45:58 +0900

    Add kernel argument to toggle on-demand paging for hugetlbfs map
    Change-Id: Id748e0a2afc4ea59142fedb652a15b4007c5dee4

--- kernel/include/page.h
+++ kernel/include/page.h
@@ -69,4 +69,5 @@ static inline int page_is_multi_mapped(struct page *page)
 
 /* Should we take page faults on ANONYMOUS mappings? */
 extern int anon_on_demand;
+extern int hugetlbfs_on_demand;
 #endif
