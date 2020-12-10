e5df1c0637b203bba0a1d5464976489cdef6a88e
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-12-10 15:45:58 +0900

    Add kernel argument to toggle on-demand paging for hugetlbfs map
    Change-Id: Id748e0a2afc4ea59142fedb652a15b4007c5dee4

--- kernel/mem.c
+++ kernel/mem.c
@@ -63,6 +63,7 @@ extern int interrupt_from_user(void *);
 struct tlb_flush_entry tlb_flush_vector[IHK_TLB_FLUSH_IRQ_VECTOR_SIZE];
 
 int anon_on_demand = 0;
+int hugetlbfs_on_demand = 1;
 int sysctl_overcommit_memory = OVERCOMMIT_ALWAYS;
 
 static struct ihk_mc_pa_ops *pa_ops;
@@ -2041,6 +2042,11 @@ void mem_init(void)
 		anon_on_demand = 1;
 	}
 	
+	if (find_command_line("hugetlbfs_on_demand")) {
+		kprintf("Demand paging on hugetlbfs mappings enabled.\n");
+		hugetlbfs_on_demand = 1;
+	}
+
 	/* Init distance vectors */
 	numa_distances_init();
 }
