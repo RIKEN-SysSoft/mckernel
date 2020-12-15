cd92f84e33db8d9afa7c4be09256303b8d564e08
Balazs Gerofi <bgerofi@riken.jp>
2020-12-15 14:43:12 +0900

    Tofu: keep track of stags per memory range
    Change-Id: I033beaeee3b141dab4485dd3a2a3848eaa84e54e

--- kernel/include/process.h
+++ kernel/include/process.h
@@ -395,6 +395,7 @@ struct vm_range {
 	off_t objoff;
 	int pgshift;	/* page size. 0 means THP */
 	int padding;
+	struct list_head tofu_stag_list;
 	void *private_data;
 };
 
@@ -764,6 +765,7 @@ struct thread {
 };
 
 #define VM_RANGE_CACHE_SIZE	4
+#define TOFU_STAG_HASH_SIZE 4
 
 struct process_vm {
 	struct address_space *address_space;
@@ -796,6 +798,10 @@ struct process_vm {
 	struct vm_range *range_cache[VM_RANGE_CACHE_SIZE];
 	int range_cache_ind;
 	struct swapinfo *swapinfo;
+
+	/* Tofu STAG hash */
+	ihk_spinlock_t tofu_stag_lock;
+	struct list_head tofu_stag_hash[TOFU_STAG_HASH_SIZE];
 };
 
 static inline int has_cap_ipc_lock(struct thread *th)
