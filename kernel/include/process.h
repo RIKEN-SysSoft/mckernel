530110e3a900f0e0d782deea8aae2017890580b3
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-12-17 17:16:13 +0900

    Tofu: fix ENABLE_TOFU switching
    Change-Id: Ib33323d4b59ea8fb4f5f40dff7ea25a36773d5e2

--- kernel/include/process.h
+++ kernel/include/process.h
@@ -567,7 +567,9 @@ struct process {
 	int thp_disable;
 
 	int straight_map;
+#ifdef ENABLE_TOFU
 	int enable_tofu;
+#endif
 	size_t straight_map_threshold;
 
 	// perf_event
@@ -592,9 +594,11 @@ struct process {
 
 	int coredump_barrier_count, coredump_barrier_count2;
 	mcs_rwlock_lock_t coredump_lock; // lock for coredump
+#ifdef ENABLE_TOFU
 #define MAX_FD_PDE 1024
 	void *fd_pde_data[MAX_FD_PDE];
 	char *fd_path[MAX_FD_PDE];
+#endif
 };
 
 /*
@@ -753,8 +757,10 @@ struct thread {
 	struct waitq coredump_wq;
 	int coredump_status;
 
+#ifdef ENABLE_TOFU
 	/* Path of file being opened */
 	char *fd_path_in_open;
+#endif
 };
 
 #define VM_RANGE_CACHE_SIZE	4
--- kernel/include/syscall.h
+++ kernel/include/syscall.h
@@ -238,7 +238,9 @@ struct program_load_desc {
 	int uti_use_last_cpu; /* Work-around not to share CPU with OpenMP thread */
 	int straight_map;
 	size_t straight_map_threshold;
+#ifdef ENABLE_TOFU
 	int enable_tofu;
+#endif
 	int nr_processes;
 	int process_rank;
 	__cpu_set_unit cpu_set[PLD_CPU_SET_SIZE];
