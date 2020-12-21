0af7aac65e92fb30fce12d8485c4cec777f2acff
Balazs Gerofi <bgerofi@riken.jp>
2020-12-21 09:48:16 +0900

    TO RESET: send SIGSTOP instead of SIGV in PF
    Change-Id: I5f7e07cb89f5f38b7c631d838f0eee0a2a98e246

--- kernel/mem.c
+++ kernel/mem.c
@@ -1453,9 +1453,9 @@ out_linux:
 		unhandled_page_fault(thread, fault_addr, reason, regs);
 		preempt_enable();
 
-		//kprintf("%s: sending SIGSTOP to TID: %d\n", __func__, thread->tid);
-		//do_kill(thread, thread->proc->pid, thread->tid, SIGSTOP, NULL, 0);
-		//goto out;
+		kprintf("%s: sending SIGSTOP to TID: %d\n", __func__, thread->tid);
+		do_kill(thread, thread->proc->pid, thread->tid, SIGSTOP, NULL, 0);
+		goto out;
 
 		memset(&info, '\0', sizeof info);
 		if (error == -ERANGE) {
