a617cd6ff1ece3ea37b007cec7c23bf76b239e84
Yoshihisa Morizumi <yoshi.morizumi@fujitsu.com>
2021-01-08 16:56:32 +0900

    xpmem: support large page
    1. try to use as large page as possible on attach
    2. pre-map resident remote pages on attach
    
    Change-Id: I5580682a4199e94085a9bad9ce3958a0f14cdcea

--- arch/arm64/kernel/syscall.c
+++ arch/arm64/kernel/syscall.c
@@ -15,6 +15,7 @@
 #include <limits.h>
 #include <uio.h>
 #include <syscall.h>
+#include <bitops.h>
 #include <rusage_private.h>
 #include <memory.h>
 #include <ihk/debug.h>
@@ -1807,7 +1808,7 @@ SYSCALL_DECLARE(mmap)
 		goto out;
 	}
 
-	addr = do_mmap(addr, len, prot, flags, fd, off0);
+	addr = do_mmap(addr, len, prot, flags, fd, off0, 0, NULL);
 
 	error = 0;
 out:
