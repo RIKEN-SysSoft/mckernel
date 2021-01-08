1dc8c0ab10623d99fbf9c09549c6810cb8592da2
Yoshihisa Morizumi <yoshi.morizumi@fujitsu.com>
2021-01-08 18:16:02 +0900

    xpmem: fix mapping of attachment and segment
    This reverts commit e774e1b9841e4b2528cf5edf1728d3072f88d674.
    
    Conflicts:
    	kernel/include/page.h
    	kernel/mem.c
    	kernel/xpmem.c
    
    Change-Id: I46e83e47435a155a6b40e25164c926c764d4bc90

--- kernel/process.c
+++ kernel/process.c
@@ -2075,7 +2075,9 @@ out:
 	return error;
 }
 
-static int page_fault_process_memory_range(struct process_vm *vm, struct vm_range *range, uintptr_t fault_addr, uint64_t reason)
+int page_fault_process_memory_range(struct process_vm *vm,
+				    struct vm_range *range,
+				    uintptr_t fault_addr, uint64_t reason)
 {
 	int error;
 	pte_t *ptep;
