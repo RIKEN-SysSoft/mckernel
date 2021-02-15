6ef2990f14eb2041c3296d7b1e44db30d9958612
Masamichi Takagi <masamichi.takagi@riken.jp>
2021-02-15 13:36:55 +0900

    stack: grow before lookup
    Change-Id: I8b39fe45520eb26409ae7404ef101a222a1eee88

--- kernel/xpmem.c
+++ kernel/xpmem.c
@@ -1212,16 +1212,24 @@ out_2:
 		goto out_1;
 	}
 
-	/* ref remote process, vm, range */
+	src_vm = seg_tg->vm;
+
+	/* grow stack before lookup */
+	ret = grow_stack(src_vm, seg_vaddr);
+	if (ret) {
+		kprintf("%s: grow_stack failed with %d\n",
+			__func__, ret);
+		goto out_1;
+	}
+
+	/* ref source thread and range */
 	src_thread = seg_tg->group_leader;
 	hold_thread(src_thread);
-
-	src_vm = seg_tg->vm;
 	hold_process_vm(src_vm);
 
 	ihk_rwspinlock_write_lock_noirq(&src_vm->memory_range_lock);
 
-	/* mark source ranges containing attached */
+	/* ref source ranges containing attached */
 	src_range = NULL;
 	for (src_vaddr = seg_vaddr; src_vaddr < seg_vaddr + size; src_vaddr = src_range->end) {
 		if (src_range == NULL) {
